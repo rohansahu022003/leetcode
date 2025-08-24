2ca8ece05acd7f2868de5bdeb22e37b83e39d2d0
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-25 02:24:04 +0530

    LC-longest-subarray-of-1s-after-deleting-one-element [Runtime: N/A; Memory: N/A]

--- sliding-window/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ sliding-window/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -8,7 +8,7 @@ public:
                 zerocount++;
             }
             if(zerocount>1){
-                if(nums[left]!=nums[left+1]){
+                if(nums[left]==0 && nums[left+1]==1){
                 left++;
                 zerocount--;
                 }

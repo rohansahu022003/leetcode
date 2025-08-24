242de9bb4636f3d640e498ee5d5e58e57bd933d3
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-25 02:47:35 +0530

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- two-pointers/move-zeroes.cpp
+++ two-pointers/move-zeroes.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=-1;
-        for(int i=0; i<nums.size();i++){
-            if(nums[i]==0){
-                 j=i;
-                break;
-            }
-            
+      int i=0,j=0;
+      int n=nums.size();
+      while(j<n){
+        if(nums[i]==0 && nums[j]!=0){
+            swap(nums[i],nums[j]);
+            i++;
+            j++;
         }
-        if(j==-1){
-         nums.size();
-        }
-        
-        else{
-        for(int i=j+1; i<nums.size(); i++){
-            if(nums[j]==0&& nums[i]!=0){
-                swap(nums[j], nums[i]);
-                j++;
-            }
-           
-
-        }
-        
-            
-        }
-        
+    
+       else j++;
+      }  
     }
 };

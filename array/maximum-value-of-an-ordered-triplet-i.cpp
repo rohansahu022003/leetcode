87f86a6d38a2a7f8684ca785982c925dea0edff9
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-26 15:29:57 +0530

    LC-maximum-value-of-an-ordered-triplet-i [Runtime: 0 ms; Memory: 21.8 MB]

--- array/maximum-value-of-an-ordered-triplet-i.cpp
+++ array/maximum-value-of-an-ordered-triplet-i.cpp
@@ -7,7 +7,10 @@ public:
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 for(int k=j+1; k<n; k++){
-                    long long value=(nums[i]-nums[j])*nums[k];
+                    long long I=nums[i];
+                    long long J=nums[j];
+                    long long K=nums[k];
+                    long long value=(I-J)*K;
                     maxvalue=max(maxvalue,value);
                 }
             }

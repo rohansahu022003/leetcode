2595526c80ec6bc0ceb4f65a609b6e5e175104aa
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-26 23:58:31 +0530

    LC-number-of-operations-to-make-network-connected [Runtime: 12 ms; Memory: 46.8 MB]

--- union-find/number-of-operations-to-make-network-connected.cpp
+++ union-find/number-of-operations-to-make-network-connected.cpp
@@ -1,35 +1,60 @@
+class disjointsets{
+vector<int>parent,size;
+public:
+
+disjointsets(int n){
+    parent.resize(n);
+    size.resize(n+1,1);
+    for (int i=0; i<n;i++){
+        parent[i]=i;
+    }
+}
+
+int ulpa(int node){
+    if(node==parent[node])return node;
+
+    return parent[node]=ulpa(parent[node]);
+}
+void unionbysize(int u, int v){
+    int up_u=ulpa(u);
+    int up_v=ulpa(v);
+    if(up_u==up_v)return;
+    else if(size[up_u]>size[up_v]){
+        parent[up_v]=up_u;
+        size[up_u]+=size[up_v];
+    }
+    else{
+        parent[up_u]=up_v;
+        size[up_v]+=size[up_u];
+    }
+}
+int components(int& count,int n){
+     for(int i=0; i<n; i++){
+            if(i==parent[i])count++;
+
+        }
+        return count;
+}
+
+};
+
+
+
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         int v=connections.size();
         if(v<n-1)return -1;
+        disjointsets ds(n);
 
-        vector<vector<int>>adj(n);
-
-        for(auto c : connections){
-            adj[c[0]].push_back(c[1]);
-            adj[c[1]].push_back(c[0]);
+        for(auto it:connections){
+            int x=it[0];
+            int y=it[1];
+            ds.unionbysize(x,y);   
         }
-        queue<int>q;
-        vector<int>vis(n,0);
         int component=0;
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-            q.push(i);
-            component++;
-            }
-            while(!q.empty()){
-                int node=q.front();
-                vis[node]=1;
-                q.pop();
-                for(auto it: adj[node]){
-                    if(!vis[it]){
-                        q.push(it);
-                    }
-                }
-            }
-        
-        }
+       
+        ds.components(component,n);
         return component-1;
     }
 };

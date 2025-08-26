5b304ee7ed1de414b794775cd58589e427f50669
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-26 16:40:05 +0530

    LC-three-consecutive-odds [Runtime: 0 ms; Memory: 11.9 MB]

---
+++ array/three-consecutive-odds.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+      int  k=0;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]%2!=0){
+                k++;
+                if(k==3){
+                    return true;
+                }
+
+            }
+            else if(arr[i]%2==0){
+                k=0;
+            }
+        }
+        return false;
+    }
+};

f3b8ad50c629b5dab50dd660e49f71e4da3d1895
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-26 15:17:55 +0530

    LC-maximum-area-of-longest-diagonal-rectangle [Runtime: 0 ms; Memory: 29.8 MB]

--- array/maximum-area-of-longest-diagonal-rectangle.cpp
+++ array/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int l,b,area;
-        float diagonal=0;
+        long long l=0,b=0,maxarea=0;
+        long long diagonal=0;
 
         for(auto it: dimensions){
-            float li=it[0];
-            float bi=it[1];
-            float root=sqrt((li*li) + (bi*bi));
+            int li=it[0];
+            int bi=it[1];
+            int root=((li*li) + (bi*bi));
+            long long area=li*bi;
             if(root>diagonal){
                 diagonal=root;
                 l=li;
                 b=bi;
+                maxarea=area;
+            }
+            else if(root==diagonal){
+                maxarea=max(maxarea,area);
             }
     }
-    return l*b;
+    return maxarea;
     }
 };

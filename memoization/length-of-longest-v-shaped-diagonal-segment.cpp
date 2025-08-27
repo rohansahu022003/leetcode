433ca0dbae1b298704fa6129f28cea8592235dcd
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-28 02:54:09 +0530

    LC-length-of-longest-v-shaped-diagonal-segment [Runtime: N/A; Memory: N/A]

--- memoization/length-of-longest-v-shaped-diagonal-segment.cpp
+++ memoization/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,11 +1,12 @@
 class Solution {
 private:
-void dfs(int row,int col,vector<int>& ans, vector<vector<int>>& vis, bool& turn,vector<int>& temp,int i,int j,vector<vector<int>>& grid){
+void dfs(int row,int col,int &maxlen, vector<vector<int>>& vis, bool turn,vector<int>& temp,int i,int j,vector<vector<int>>& grid){
     int n=grid.size();
     int m=grid[0].size();
     vis[row][col]=1;
     
     temp.push_back(grid[row][col]);
+    maxlen=max(maxlen,(int)temp.size());
     
     int drow[]={1,1,-1,-1};
     int dcol[]={1,-1,1,-1};
@@ -15,45 +16,45 @@ void dfs(int row,int col,vector<int>& ans, vector<vector<int>>& vis, bool& turn,
             int nrow=row+drow[i];
             int ncol=col+dcol[i];
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==2){
-                dfs(nrow,ncol,ans,vis,turn,temp,nrow-row,ncol-col,grid);
+                dfs(nrow,ncol,maxlen,vis,turn,temp,nrow-row,ncol-col,grid);
             }
         }
     }
     if(grid[row][col]==2){
     if(i==-1 && j==-1){
           if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==0 && !vis[row-1][col-1]){
-            dfs(row-1,col-1,ans,vis,turn,temp,-1,-1,grid);
+            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
         }
-        else if(row-1>=0 && col+1<m && grid[row-1][col+1]==0 && !vis[row-1][col+1] && turn==false){
+         if(row-1>=0 && col+1<m && grid[row-1][col+1]==0 && !vis[row-1][col+1] && turn==false){
             turn=true;
-            dfs(row-1,col+1,ans,vis,turn,temp,-1,1,grid);
+            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
         }
     }
     else if(i==-1 && j==1){
          if(row-1>=0 && col+1<m && grid[row-1][col+1]==0 && !vis[row-1][col+1]){
-            dfs(row-1,col+1,ans,vis,turn,temp,-1,1,grid);
+            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
         }
-        else if(row+1<n && col+1<m && grid[row+1][col+1]==0 && !vis[row+1][col+1] && turn==false){
+         if(row+1<n && col+1<m && grid[row+1][col+1]==0 && !vis[row+1][col+1] && turn==false){
             turn=true;
-            dfs(row+1,col+1,ans,vis,turn,temp,1,1,grid);
+            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
         }
     }
     else if(i==1 && j==1){
          if(row+1<n && col+1<m && grid[row+1][col+1]==0 && !vis[row+1][col+1]){
-            dfs(row+1,col+1,ans,vis,turn,temp,1,1,grid);
+            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
         }
-        else if(row+1<n && col-1>=0 && grid[row+1][col-1]==0 && !vis[row+1][col-1] && turn==false){
+         if(row+1<n && col-1>=0 && grid[row+1][col-1]==0 && !vis[row+1][col-1] && turn==false){
             turn=true;
-            dfs(row+1,col-1,ans,vis,turn,temp,1,-1,grid);
+            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
         }
     }
     else if(i==1 && j==-1){
         if(row+1<n && col-1>=0 && grid[row+1][col-1]==0 && !vis[row+1][col-1]){
-            dfs(row+1,col-1,ans,vis,turn,temp,1,-1,grid);
+            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
         }
-        else if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==0 && !vis[row-1][col-1] && turn==false){
+        if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==0 && !vis[row-1][col-1] && turn==false){
             turn=true;
-            dfs(row-1,col-1,ans,vis,turn,temp,-1,-1,grid);
+            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
         }
 
     }
@@ -62,48 +63,48 @@ void dfs(int row,int col,vector<int>& ans, vector<vector<int>>& vis, bool& turn,
     else if(grid[row][col]==0){
          if(i==-1 && j==-1){
           if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==2 && !vis[row-1][col-1]){
-            dfs(row-1,col-1,ans,vis,turn,temp,-1,-1,grid);
+            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
         }
-        else if(row-1>=0 && col+1<m && grid[row-1][col+1]==2 && !vis[row-1][col+1] && turn==false){
+         if(row-1>=0 && col+1<m && grid[row-1][col+1]==2 && !vis[row-1][col+1] && turn==false){
             turn=true;
-            dfs(row-1,col+1,ans,vis,turn,temp,-1,1,grid);
+            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
         }
     }
     else if(i==-1 && j==1){
          if(row-1>=0 && col+1<m && grid[row-1][col+1]==2 && !vis[row-1][col+1]){
-            dfs(row-1,col+1,ans,vis,turn,temp,-1,1,grid);
+            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
         }
-        else if(row+1<n && col+1<m && grid[row+1][col+1]==2 && !vis[row+1][col+1] && turn==false){
+         if(row+1<n && col+1<m && grid[row+1][col+1]==2 && !vis[row+1][col+1] && turn==false){
             turn=true;
-            dfs(row+1,col+1,ans,vis,turn,temp,1,1,grid);
+            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
         }
     }
     else if(i==1 && j==1){
          if(row+1<n && col+1<m && grid[row+1][col+1]==2 && !vis[row+1][col+1]){
-            dfs(row+1,col+1,ans,vis,turn,temp,1,1,grid);
+            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
         }
-        else if(row+1<n && col-1>=0 && grid[row+1][col-1]==2 && !vis[row+1][col-1] && turn==false){
+         if(row+1<n && col-1>=0 && grid[row+1][col-1]==2 && !vis[row+1][col-1] && turn==false){
             turn=true;
-            dfs(row+1,col-1,ans,vis,turn,temp,1,-1,grid);
+            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
         }
     }
     else if(i==1 && j==-1){
         if(row+1<n && col-1>=0 && grid[row+1][col-1]==2 && !vis[row+1][col-1]){
-            dfs(row+1,col-1,ans,vis,turn,temp,1,-1,grid);
+            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
         }
-        else if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==2 && !vis[row-1][col-1] && turn==false){
+         if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==2 && !vis[row-1][col-1] && turn==false){
             turn=true;
-            dfs(row-1,col-1,ans,vis,turn,temp,-1,-1,grid);
+            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
         }
 
     }
     }
-    if(temp.size()>ans.size()){
-     ans=temp;
-     temp.clear();
-     
+    
+   temp.pop_back();
+   vis[row][col]=0;
+
       
-    }
+    
 }
 
 public:
@@ -111,20 +112,21 @@ public:
         int n=grid.size();
         int m=grid[0].size();
 
-        vector<int>ans;
+        int maxlen=0;
        
-        bool turn=false;
+       
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==1){
+                    bool turn=false;
                     vector<vector<int>>vis(n,vector<int>(m,0));
                     vector<int>temp;
-                    dfs(i,j,ans,vis,turn,temp,0,0,grid);
+                    dfs(i,j,maxlen,vis,turn,temp,0,0,grid);
                 }
             }
         }
-        return ans.size();
+        return maxlen;
 
     }
 };

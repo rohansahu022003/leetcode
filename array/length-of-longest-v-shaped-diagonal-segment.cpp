841164dfd0453f8661278491b16621154f3d9e89
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-28 02:55:20 +0530

    LC-length-of-longest-v-shaped-diagonal-segment [Runtime: N/A; Memory: N/A]

--- array/length-of-longest-v-shaped-diagonal-segment.cpp
+++ array/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,132 +1,64 @@
 class Solution {
 private:
-void dfs(int row,int col,int &maxlen, vector<vector<int>>& vis, bool turn,vector<int>& temp,int i,int j,vector<vector<int>>& grid){
-    int n=grid.size();
-    int m=grid[0].size();
-    vis[row][col]=1;
-    
-    temp.push_back(grid[row][col]);
-    maxlen=max(maxlen,(int)temp.size());
-    
-    int drow[]={1,1,-1,-1};
-    int dcol[]={1,-1,1,-1};
+    void dfs(int row, int col, int& maxLen,
+             vector<vector<int>>& vis,
+             bool turn, vector<int>& temp,
+             int i, int j, vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vis[row][col] = 1;
+        temp.push_back(grid[row][col]);
+        maxLen = max(maxLen, (int)temp.size());
 
-    if(grid[row][col]==1){
-        for(int i=0; i<4; i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==2){
-                dfs(nrow,ncol,maxlen,vis,turn,temp,nrow-row,ncol-col,grid);
-            }
-        }
-    }
-    if(grid[row][col]==2){
-    if(i==-1 && j==-1){
-          if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==0 && !vis[row-1][col-1]){
-            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
-        }
-         if(row-1>=0 && col+1<m && grid[row-1][col+1]==0 && !vis[row-1][col+1] && turn==false){
-            turn=true;
-            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
-        }
-    }
-    else if(i==-1 && j==1){
-         if(row-1>=0 && col+1<m && grid[row-1][col+1]==0 && !vis[row-1][col+1]){
-            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
-        }
-         if(row+1<n && col+1<m && grid[row+1][col+1]==0 && !vis[row+1][col+1] && turn==false){
-            turn=true;
-            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
-        }
-    }
-    else if(i==1 && j==1){
-         if(row+1<n && col+1<m && grid[row+1][col+1]==0 && !vis[row+1][col+1]){
-            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
-        }
-         if(row+1<n && col-1>=0 && grid[row+1][col-1]==0 && !vis[row+1][col-1] && turn==false){
-            turn=true;
-            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
-        }
-    }
-    else if(i==1 && j==-1){
-        if(row+1<n && col-1>=0 && grid[row+1][col-1]==0 && !vis[row+1][col-1]){
-            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
-        }
-        if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==0 && !vis[row-1][col-1] && turn==false){
-            turn=true;
-            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
-        }
+        int drow[] = {1, 1, -1, -1};
+        int dcol[] = {1, -1, 1, -1};
 
-    }
-    }
-
-    else if(grid[row][col]==0){
-         if(i==-1 && j==-1){
-          if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==2 && !vis[row-1][col-1]){
-            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
-        }
-         if(row-1>=0 && col+1<m && grid[row-1][col+1]==2 && !vis[row-1][col+1] && turn==false){
-            turn=true;
-            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
-        }
-    }
-    else if(i==-1 && j==1){
-         if(row-1>=0 && col+1<m && grid[row-1][col+1]==2 && !vis[row-1][col+1]){
-            dfs(row-1,col+1,maxlen,vis,turn,temp,-1,1,grid);
-        }
-         if(row+1<n && col+1<m && grid[row+1][col+1]==2 && !vis[row+1][col+1] && turn==false){
-            turn=true;
-            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
-        }
-    }
-    else if(i==1 && j==1){
-         if(row+1<n && col+1<m && grid[row+1][col+1]==2 && !vis[row+1][col+1]){
-            dfs(row+1,col+1,maxlen,vis,turn,temp,1,1,grid);
-        }
-         if(row+1<n && col-1>=0 && grid[row+1][col-1]==2 && !vis[row+1][col-1] && turn==false){
-            turn=true;
-            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
-        }
-    }
-    else if(i==1 && j==-1){
-        if(row+1<n && col-1>=0 && grid[row+1][col-1]==2 && !vis[row+1][col-1]){
-            dfs(row+1,col-1,maxlen,vis,turn,temp,1,-1,grid);
-        }
-         if(row-1>=0 && col-1>=0 && grid[row-1][col-1]==2 && !vis[row-1][col-1] && turn==false){
-            turn=true;
-            dfs(row-1,col-1,maxlen,vis,turn,temp,-1,-1,grid);
-        }
-
-    }
-    }
-    
-   temp.pop_back();
-   vis[row][col]=0;
-
-      
-    
-}
-
-public:
-    int lenOfVDiagonal(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-
-        int maxlen=0;
-       
-       
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==1){
-                    bool turn=false;
-                    vector<vector<int>>vis(n,vector<int>(m,0));
-                    vector<int>temp;
-                    dfs(i,j,maxlen,vis,turn,temp,0,0,grid);
+        if (grid[row][col] == 1) {
+            for (int k = 0; k < 4; k++) {
+                int nrow = row + drow[k];
+                int ncol = col + dcol[k];
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 2) {
+                    dfs(nrow, ncol, maxLen, vis, turn, temp, nrow - row, ncol - col, grid);
                 }
             }
         }
-        return maxlen;
+        else if (grid[row][col] == 2 || grid[row][col] == 0) {
+            int expected = (grid[row][col] == 2 ? 0 : 2);
+            int nrow = row + i;
+            int ncol = col + j;
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == expected && !vis[nrow][ncol]) {
+                dfs(nrow, ncol, maxLen, vis, turn, temp, i, j, grid);
+            }
+            if (!turn) { // allow one clockwise turn
+                int ni = j, nj = -i; // clockwise rotation
+                nrow = row + ni;
+                ncol = col + nj;
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == expected && !vis[nrow][ncol]) {
+                    dfs(nrow, ncol, maxLen, vis, true, temp, ni, nj, grid);
+                }
+            }
+        }
 
+        temp.pop_back();
+        vis[row][col] = 0; // backtrack
+    }
+
+public:
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int maxLen = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    vector<vector<int>> vis(n, vector<int>(m, 0));
+                    vector<int> temp;
+                    dfs(i, j, maxLen, vis, false, temp, 0, 0, grid);
+                }
+            }
+        }
+        return maxLen;
     }
 };

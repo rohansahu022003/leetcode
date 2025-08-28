7a0b4cdaa17068c890934a94f7fa69dfa49a98b4
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-29 02:40:18 +0530

    LC-maximum-unique-subarray-sum-after-deletion [Runtime: 0 ms; Memory: 29.5 MB]

--- hash-table/maximum-unique-subarray-sum-after-deletion.cpp
+++ hash-table/maximum-unique-subarray-sum-after-deletion.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int maxSum(vector<int>& nums) {
         set<int>st(nums.begin(), nums.end());
-        int maxsum=0;
-        int sum=0;
+        int maxsum=*st.begin();
+        int sum=*st.begin();
         for(auto s: st){
-            if(s>=0){
-            sum+=s;
+            if(s!=*st.begin()){
+            sum=max(s,sum+s);
             maxsum=max(maxsum,sum);
             }
+            
         }
         return maxsum;
     }

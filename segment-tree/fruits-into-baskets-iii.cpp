2bda208021383f1e6160ebc86ff769310677563f
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-29 01:57:06 +0530

    LC-fruits-into-baskets-iii [Runtime: N/A; Memory: N/A]

--- segment-tree/fruits-into-baskets-iii.cpp
+++ segment-tree/fruits-into-baskets-iii.cpp
@@ -4,8 +4,10 @@ public:
         int n=fruits.size();
         int count=0;
         int i=0;
+        sort(baskets.begin(), baskets.end());
         while(i<n){
-            for(int j=0; j<n; j++){
+            if(fruits[i]<=baskets[n/2]){
+                for(int j=0; j<=n/2; j++){
                 if(fruits[i]<=baskets[j]){
                     count++;
                     baskets[j]=-1;
@@ -13,6 +15,18 @@ public:
 
                 }
 
+
+                }  
+            }
+            else{
+                for(int j=n/2+1; j<n; j++){
+                if(fruits[i]<=baskets[j]){
+                    count++;
+                    baskets[j]=-1;
+                    break;
+
+            }
+                }
             }
             i++;
         }

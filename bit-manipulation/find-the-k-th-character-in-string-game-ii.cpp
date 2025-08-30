cb1281e0049f14286bd1b44c8cb69e39eaaf011e
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-30 20:34:47 +0530

    LC-find-the-k-th-character-in-string-game-ii [Runtime: N/A; Memory: N/A]

--- bit-manipulation/find-the-k-th-character-in-string-game-ii.cpp
+++ bit-manipulation/find-the-k-th-character-in-string-game-ii.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     char kthCharacter(long long k, vector<int>& operations) {
        char c='a';
+      long long len=1;
 
-       for(int i=operations.size()-1; i>=0;i--){
-        long long half= 1LL<<i;
+
+       for(int i=0; i<operations.size(); i++){
+        if(len>k)break;
+        len*=2;
+       }
+
+       for(int i=operations.size()-1; i>=0; i--){
+        long long half= len/2;
         if(k>half){
             k-=half;
             if(operations[i]==1){
-                c=(c=='z'?'a':c+1);
+                c=(c=='z'?'a':c+=1);
             }
+            
         }
+        len /=2;
        }
         return c;
        }

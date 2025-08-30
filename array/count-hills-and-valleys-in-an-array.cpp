b88ab61e617d13ca8bd56a5a3c5148f20da01491
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-30 15:34:43 +0530

    LC-count-hills-and-valleys-in-an-array [Runtime: N/A; Memory: N/A]

--- array/count-hills-and-valleys-in-an-array.cpp
+++ array/count-hills-and-valleys-in-an-array.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
-        int n=nums.size();
-        int i=1;
-        int k=2;
-        int j=0;
-        int count=0;
+    vector<int>arr;
+    int count=0;
 
-        while(k<n){
-            while(nums[i]==nums[j]&& nums[j]==nums[k]){
-                i+=2;
-                j+=2;
-                k+=2;
-            }
-            while(nums[i]==nums[j]){
-                i++;
-                j++;
-                k++;
-            }
-            while(nums[i]==nums[k]){
-                i++;
-                k++;
-            }
-            while(nums[j]==nums[j+1]){
-                j++;
-            }
-            if((nums[j]<nums[i] && nums[i]>nums[k]) || (nums[j]>nums[i] && nums[i]<nums[k])){
-                count++;
-                
-            }
-                j++;
-                i++;
-                k++;
-            
-
-        } 
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i]!=nums[i-1]){
+            arr.push_back(nums[i]);
+        }
+    }
+    for(int i=1; i<n-1; i++){
+        if((nums[i-1]<nums[i] && nums[i]>nums[i+1]) || (nums[i-1]>nums[i] && nums[i]<nums[i+1])){
+            count++;
+        }
+    }
         return count;
 
     }

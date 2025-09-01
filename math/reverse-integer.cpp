0adee130b40015f0963a679965851660ed6bd4cf
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-02 01:19:10 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.cpp
+++ math/reverse-integer.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int reverse(int x) {
-        int y;
-        if(x<0)
-        {y=-(x);
-        }
-        else if(x>0) {
-            y=x;
-        }
-        
        int n=0;
-        while(y>0){
-            int digit=y%10;
-            y/=10;
+        while(x!=0){
+            int digit=x%10;
+            x/=10;
             if(n>=INT_MAX/10 || n<=INT_MIN/10){
                 return 0;
             }

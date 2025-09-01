7ff9d7d3a7106afea5452fdc5d6dabdcb877ee36
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-01 13:07:34 +0530

    LC-maximum-average-pass-ratio [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-average-pass-ratio.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<pair<double,int>>pq;
+
+        for(int i=0; i<classes.size(); i++){
+            int pass=classes[i][0], total=classes[i][1];
+            double gain= (double)(pass+1)/(total+1) -(double)pass/total;
+            pq.push({gain,i});
+        }
+        while(extraStudents--){
+            auto it= pq.top();
+            int i =it.second;
+            pq.pop();
+                double newgain= (double)(classes[i][0]+1)/(classes[i][1]+1) - (double)classes[i][0]/classes[i][1];
+                classes[i][0]++;
+                classes[i][1]++;
+                pq.push({newgain,i});
+        }
+        double sum=0;
+
+        for(auto c: classes){
+            sum+=(double)c[0]/c[1];
+        }
+        return sum/classes.size();
+    }
+};

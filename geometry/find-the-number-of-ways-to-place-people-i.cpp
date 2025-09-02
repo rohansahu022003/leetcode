268a14acfc9b36ffdcebcd3c9862eaa7ad45ed5a
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-02 10:08:53 +0530

    LC-find-the-number-of-ways-to-place-people-i [Runtime: 11 ms; Memory: 32.6 MB]

--- geometry/find-the-number-of-ways-to-place-people-i.cpp
+++ geometry/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    private:
-   static bool cmp(vector<int>p, vector<int>q){
-    return (p[0]==q[0]?p[1]<q[1]:p[0]>q[0]);
-   }
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-       sort(points.begin(), points.end(),cmp);
-       int count=0;
-       for(int i=0; i<points.size()-1; i++){
-        int y=INT_MAX;
-        for(int j=i+1; j<points.size(); j++){
-           if(points[j][1]>=points[i][1] && y>points[j][1]){
-                count++;
-                y=points[j][1];
-            
+        sort(points.begin(), points.end(), [](const auto &a, const auto &b) { return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]; });
+
+        int res = 0, n = points.size();
+        for (int i = 0; i < n; i++)
+        {
+            int b = points[i][1], last = -1;
+            for (int j = i + 1; j < n; j++)
+            {
+                int a = points[j][1];
+                if (a <= b && a > last)
+                {
+                    last = a;
+                    ++res;
+                }
             }
-            
-           }
         }
-       
-       return count;
+
+        return res;
     }
 };

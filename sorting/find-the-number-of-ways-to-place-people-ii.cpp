f891b4cc2b25383e4bba3e5314078718d4f82163
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-03 03:24:50 +0530

    LC-find-the-number-of-ways-to-place-people-ii [Runtime: N/A; Memory: N/A]

---
+++ sorting/find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& p) {
+        sort(p.begin(), p.end(), [](const auto &a, const auto &b )
+        {return a[0]==b[0]?a[1]>b[1]: a[0]<b[0];});
+
+        int count=0;
+        
+        for(int i=0; i<p.size(); i++){
+            int a=p[i][1];
+            int y=-1;
+            for(int j=i+1; j<p.size(); j++){
+                int b=p[j][1];
+                if(a>=b && b>y){
+                    count++;
+                    y=b;
+                }
+            }
+        }
+        return count;
+    }
+};

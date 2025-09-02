5d270e18d3c8dfb18792c139ba6409414dc1c3ce
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-02 10:06:26 +0530

    LC-find-the-number-of-ways-to-place-people-i [Runtime: 36 ms; Memory: 41.4 MB]

--- sorting/find-the-number-of-ways-to-place-people-i.cpp
+++ sorting/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,28 +1,19 @@
 class Solution {
     private:
-    bool nopoint(int i,int j,vector<vector<int>>& points){
-        for(int k=i+1; k<j; k++){
-            {
-            if(points[i][0] <= points[k][0] && points[k][0] <= points[j][0] &&
-   points[i][1] >= points[k][1] && points[k][1] >= points[j][1]){
-                return false;
-            }
-            }
-        }
-        return true;
-
-    }
+   static bool cmp(vector<int>p, vector<int>q){
+    return (p[0]==q[0]?p[1]<q[1]:p[0]>q[0]);
+   }
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-       sort(points.begin(), points.end());
+       sort(points.begin(), points.end(),cmp);
        int count=0;
        for(int i=0; i<points.size()-1; i++){
+        int y=INT_MAX;
         for(int j=i+1; j<points.size(); j++){
-           if(points[i][1]>=points[j][1]){
-           
-            if(nopoint(i,j,points)){
+           if(points[j][1]>=points[i][1] && y>points[j][1]){
                 count++;
-            }
+                y=points[j][1];
+            
             }
             
            }

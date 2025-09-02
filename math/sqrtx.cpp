d75930974d0c6c1a3e9c9bb9fc027eda1520acbf
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-03 04:05:38 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.cpp
+++ math/sqrtx.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+       if(x<2)return 1;
+
+       int i=2;
+       while(i*i<=x){
+        i++;
+       }
+       return i-1;
     }
 };

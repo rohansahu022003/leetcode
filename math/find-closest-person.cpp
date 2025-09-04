6de2ba2957d70fbce0c19c16f858b6a06dfcf805
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-05 01:56:52 +0530

    LC-find-closest-person [Runtime: 0 ms; Memory: 8.7 MB]

---
+++ math/find-closest-person.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if(abs(x-z)>abs(y-z))return 2;
+        else if(abs(y-z)>abs(x-z))return 1;
+        else return 0;
+    }
+};

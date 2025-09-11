449d95b9574497103a924f6fb80afdfffa5368da
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-11 07:15:32 +0530

    LC-plus-one [Runtime: N/A; Memory: N/A]

---
+++ array/plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int>ans;
+        int num=0;
+        for(int i=0; i<digits.size();i++){
+            num=num*10+digits[i];
+        }
+        num++;
+        while(num){
+            int d=num%10;
+            ans.push_back(d);
+            num/=10;
+
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};

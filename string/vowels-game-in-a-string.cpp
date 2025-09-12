a4bf52ddfa8a65b369b80688376477d0b1a876b1
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-12 06:46:23 +0530

    LC-vowels-game-in-a-string [Runtime: 10 ms; Memory: 19.3 MB]

--- string/vowels-game-in-a-string.cpp
+++ string/vowels-game-in-a-string.cpp
@@ -1,24 +1,13 @@
 class Solution {
-    private:
-    int isvowel(string s,int& count){
-        static bool isVowel[256]={false};
-        bool initialized=false;
-        if(!initialized){
-             string vowel="aeiou";
-             for(auto v:vowel)isVowel[v]=true;
-             initialized=true;
-
-        }
-       for(auto c:s)if(isVowel[c]==true)count++;
-
-       return count;
-        
-    }
+   
 public:
     bool doesAliceWin(string s) {
-        int count=0;
-       isvowel(s,count);
-        if(count==0)return false;
-        return true;
+      string vowel="aeiou";
+
+      for(auto c:s){
+        if(vowel.find(c)!=string::npos)return true;
+      }
+       
+        return false;
     }
 };

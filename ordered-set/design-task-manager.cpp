4bb71ac93507e6758f2b4cb1e6b3049efe7b59fd
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-18 10:16:29 +0530

    LC-design-task-manager [Runtime: N/A; Memory: N/A]

--- ordered-set/design-task-manager.cpp
+++ ordered-set/design-task-manager.cpp
@@ -1,39 +1,39 @@
 class TaskManager {
-    unordered_map<int,pair<int,int>>taskto;
-    priority_queue<tuple<int,int,int>>pq;
+    unordered_map<int,pair<int,int>> taskto; // taskId -> {userId, priority}
+    priority_queue<tuple<int,int,int>> pq;   // (priority, taskId, userId)
+
 public:
     TaskManager(vector<vector<int>>& tasks) {
-       for(int i=0; i<tasks.size(); i++){
-        taskto[tasks[i][1]]={tasks[i][0],tasks[i][2]};
-        pq.push({tasks[i][2],tasks[i][1],tasks[i][0]});
-       } 
+        for (auto &t : tasks) {
+            int userId = t[0], taskId = t[1], priority = t[2];
+            taskto[taskId] = {userId, priority};
+            pq.push({priority, taskId, userId});
+        }
     }
     
     void add(int userId, int taskId, int priority) {
-      taskto[taskId]={userId,priority};
-        pq.push({priority,taskId,userId});
+        taskto[taskId] = {userId, priority};
+        pq.push({priority, taskId, userId});
     }
     
     void edit(int taskId, int newPriority) {
-       taskto[taskId].second=newPriority;
-       int userid=taskto[taskId].first;
-       pq.push({newPriority, taskId,userid});
-
-        
+        int userId = taskto[taskId].first;
+        taskto[taskId].second = newPriority;
+        pq.push({newPriority, taskId, userId});
     }
     
     void rmv(int taskId) {
-       taskto.erase(taskId);
+        taskto.erase(taskId); // lazy delete
     }
     
     int execTop() {
-       while(!pq.empty()){
-        auto [p,t,u]=pq.top();
-        pq.pop();
-        if(taskto.find(t)!=taskto.end() && taskto[t].second==p)return u;
-       }
-         return -1;
-        
+        while (!pq.empty()) {
+            auto [p, t, u] = pq.top();
+            pq.pop();
+            if (taskto.find(t) != taskto.end() && taskto[t].second == p) {
+                return u;
+            }
+        }
+        return -1; 
     }
-  
 };

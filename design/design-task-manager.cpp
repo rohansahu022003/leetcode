580e3ca12951730c1cadf003454a126a4c41f348
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-18 09:56:18 +0530

    LC-design-task-manager [Runtime: 398 ms; Memory: 389.3 MB]

--- design/design-task-manager.cpp
+++ design/design-task-manager.cpp
@@ -1,14 +1,14 @@
 class TaskManager {
     unordered_map<int,int>tasktouser;
     unordered_map<int,int>tasktopriority;
-    unordered_map<int,set<pair<int,int>>>usertopriority;
+    // unordered_map<int,set<pair<int,int>>>usertopriority;
     set<pair<int,int>,greater<>>globalranking;
 public:
     TaskManager(vector<vector<int>>& tasks) {
        for(int i=0; i<tasks.size(); i++){
         tasktouser[tasks[i][1]]=tasks[i][0];
         tasktopriority[tasks[i][1]]=tasks[i][2];
-        usertopriority[tasks[i][0]].insert({-tasks[i][2],tasks[i][1]});
+        // usertopriority[tasks[i][0]].insert({-tasks[i][2],tasks[i][1]});
         globalranking.insert({tasks[i][2],tasks[i][1]});
        } 
     }
@@ -16,7 +16,7 @@ public:
     void add(int userId, int taskId, int priority) {
         tasktouser[taskId]=userId;
         tasktopriority[taskId]=priority;
-        usertopriority[userId].insert({-priority,taskId});
+        // usertopriority[userId].insert({-priority,taskId});
         globalranking.insert({priority,taskId});
     }
     
@@ -24,8 +24,8 @@ public:
         int userid=tasktouser[taskId];
         int oldpriority=tasktopriority[taskId];
         tasktopriority[taskId]=newPriority;
-        usertopriority[userid].erase({-oldpriority,taskId});
-        usertopriority[userid].insert({-newPriority,taskId});
+        // usertopriority[userid].erase({-oldpriority,taskId});
+        // usertopriority[userid].insert({-newPriority,taskId});
         globalranking.erase({oldpriority,taskId});
         globalranking.insert({newPriority,taskId});
 
@@ -37,7 +37,7 @@ public:
         int p=tasktopriority[taskId];
         tasktouser.erase(taskId);
         tasktopriority.erase(taskId);
-        usertopriority[userid].erase({-p, taskId});
+        // usertopriority[userid].erase({-p, taskId});
         globalranking.erase({p,taskId});
     }
     
@@ -50,12 +50,3 @@ public:
         
     }
 };
-
-/**
- * Your TaskManager object will be instantiated and called as such:
- * TaskManager* obj = new TaskManager(tasks);
- * obj->add(userId,taskId,priority);
- * obj->edit(taskId,newPriority);
- * obj->rmv(taskId);
- * int param_4 = obj->execTop();
- */

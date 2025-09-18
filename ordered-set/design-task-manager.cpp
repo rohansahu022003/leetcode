0db7944538b4d903d19420a801821774a657cdaf
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-18 10:24:19 +0530

    LC-design-task-manager [Runtime: 202 ms; Memory: 351.2 MB]

--- ordered-set/design-task-manager.cpp
+++ ordered-set/design-task-manager.cpp
@@ -1,49 +1,42 @@
-
 class TaskManager {
+    unordered_map<int,pair<int,int>>taskto;
+    priority_queue<tuple<int,int,int>>pq;
 public:
-    unordered_map<int, pair<int,int>> mp;
-    priority_queue<tuple<int, int, int>> pq;
     TaskManager(vector<vector<int>>& tasks) {
-        for(auto x : tasks){
-            mp[x[1]] = {x[0], x[2]};
-            pq.push({x[2], x[1], x[0]});
-        }
+       for(int i=0; i<tasks.size(); i++){
+        taskto[tasks[i][1]]={tasks[i][0],tasks[i][2]};
+        pq.push({tasks[i][2],tasks[i][1],tasks[i][0]});
+       } 
     }
     
     void add(int userId, int taskId, int priority) {
-        mp[taskId] = {userId, priority};
-        pq.push({priority, taskId, userId});
+      taskto[taskId]={userId,priority};
+        pq.push({priority,taskId,userId});
     }
     
     void edit(int taskId, int newPriority) {
-        mp[taskId].second = newPriority;
-        int userId = mp[taskId].first;
-        pq.push({newPriority, taskId, userId});
+       taskto[taskId].second=newPriority;
+       int userid=taskto[taskId].first;
+       pq.push({newPriority, taskId,userid});
+
         
     }
     
     void rmv(int taskId) {
-        mp.erase(taskId);
+       taskto.erase(taskId);
     }
     
     int execTop() {
-        while(!pq.empty()){
-            auto [priority, taskId, userId] = pq.top();
-            pq.pop();
-            if(mp.find(taskId) != mp.end() && mp[taskId].second == priority){
-                mp.erase(taskId);
-                return userId;
-            }
+       while(!pq.empty()){
+        auto [p,t,u]=pq.top();
+        pq.pop();
+        if(taskto.find(t)!=taskto.end() && taskto[t].second==p){
+            taskto.erase(t);
+            return u;
         }
-        return  -1;
+       }
+         return -1;
+        
     }
+  
 };
-
-/**
- * Your TaskManager object will be instantiated and called as such:
- * TaskManager* obj = new TaskManager(tasks);
- * obj->add(userId,taskId,priority);
- * obj->edit(taskId,newPriority);
- * obj->rmv(taskId);
- * int param_4 = obj->execTop();
- */

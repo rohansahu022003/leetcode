9e3a2db0fa52175b4b87b65eeb1a2de2d3e567d7
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-20 09:00:20 +0530

    LC-implement-router [Runtime: 312 ms; Memory: 440.3 MB]

--- queue/implement-router.cpp
+++ queue/implement-router.cpp
@@ -1,76 +1,50 @@
-struct TupleHash {
-    size_t operator()(const tuple<int,int,int>& t) const {
-        auto [a,b,c] = t;
-        return hash<int>()(a) ^ (hash<int>()(b) << 1) ^ (hash<int>()(c) << 2);
-    }
-};
-
 class Router {
-    queue<tuple<int,int,int>> routequeue;
-    unordered_set<tuple<int,int,int>, TupleHash> routeset;
-    unordered_multimap<int, pair<int,int>> routemap;
-    int k=0;
-
 public:
-    Router(int memoryLimit) {
-        k = memoryLimit;
+    map<vector<int>, int> mpp; 
+    queue<vector<int>> queue; 
+    unordered_map<int, vector<int>> timestamps; 
+    unordered_map<int, int> st; 
+    int maxSize = 0; 
+
+    Router(int memoryLimit) { 
+        maxSize = memoryLimit; 
     }
 
     bool addPacket(int source, int destination, int timestamp) {
-        if (routeset.find(make_tuple(source,destination,timestamp)) != routeset.end()) return false;
-
-        routequeue.push(make_tuple(source,destination,timestamp));
-        routeset.insert(make_tuple(source,destination,timestamp));
-        routemap.emplace(source, make_pair(destination, timestamp));
-
-        if (routequeue.size() > (size_t)k) {
-            auto old = routequeue.front();
-            routequeue.pop();
-            routeset.erase(old);
-
-            int s = get<0>(old);
-            int d = get<1>(old);
-            int t = get<2>(old);
-
-            auto range = routemap.equal_range(s);
-            for (auto it = range.first; it != range.second; ++it) {
-                if (it->second.first == d && it->second.second == t) {
-                    routemap.erase(it);
-                    break;
-                }
-            }
+        vector<int> packet = {source, destination, timestamp};
+        
+        if (mpp.count(packet))
+            return false;
+        if (queue.size() == maxSize) { 
+            vector<int> res = queue.front();
+            mpp.erase(res);
+            int temp = res[1];
+            st[temp]++;  
+            queue.pop();
         }
-
+        queue.push(packet);
+        mpp[packet]++;
+        timestamps[destination].push_back(timestamp);
         return true;
     }
 
     vector<int> forwardPacket() {
-        if (routequeue.empty()) return {};
-        auto [s,d,t] = routequeue.front();
-        routequeue.pop();
-
-        routeset.erase(make_tuple(s,d,t));
-
-        auto range = routemap.equal_range(s);
-        for (auto it = range.first; it != range.second; ++it) {
-            if (it->second.first == d && it->second.second == t) {
-                routemap.erase(it);
-                break;
-            }
-        }
-
-        return {s,d,t};
+        if(queue.empty()) return {};
+        vector<int> res = queue.front();
+        queue.pop();
+        mpp.erase(res);
+        int temp = res[1];
+        st[temp]++;
+        return res;
     }
 
     int getCount(int destination, int startTime, int endTime) {
-        int count = 0;
-        for (auto &m : routemap) {
-            int dest = m.second.first;
-            int time = m.second.second;
-            if (dest == destination && startTime <= time && time <= endTime) {
-                count++;
-            }
-        }
-        return count;
+        if(timestamps.find(destination) == timestamps.end())
+            return 0;
+        auto &p = timestamps[destination];
+        int temp = st[destination];
+        auto right = lower_bound(p.begin() + temp, p.end(), startTime);
+        auto left = upper_bound(p.begin() + temp, p.end(), endTime);
+        return int(left - right);
     }
 };

b028b2b7ae4d023bf191a84d2e550982ac88103d
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-20 08:56:56 +0530

    LC-implement-router [Runtime: N/A; Memory: N/A]

--- hash-table/implement-router.cpp
+++ hash-table/implement-router.cpp
@@ -4,53 +4,73 @@ struct TupleHash {
         return hash<int>()(a) ^ (hash<int>()(b) << 1) ^ (hash<int>()(c) << 2);
     }
 };
+
 class Router {
-    queue<tuple<int,int,int>>routequeue;
-    unordered_set<tuple<int,int,int>,TupleHash>routeset;
-    unordered_map<int,pair<int,int>>routemap;
+    queue<tuple<int,int,int>> routequeue;
+    unordered_set<tuple<int,int,int>, TupleHash> routeset;
+    unordered_multimap<int, pair<int,int>> routemap;
     int k=0;
+
 public:
     Router(int memoryLimit) {
-        k=memoryLimit;
+        k = memoryLimit;
     }
-    
+
     bool addPacket(int source, int destination, int timestamp) {
-        if(routeset.find({source,destination,timestamp})!=routeset.end())return false;
-        routequeue.push({source,destination,timestamp});
-        routemap[source]={destination,timestamp};
-        routeset.insert({source,destination,timestamp});
-        if(routequeue.size()>k){
-            int it=get<0>(routequeue.front());
-            routemap.erase(it);
-            routeset.erase(routequeue.front());
+        if (routeset.find(make_tuple(source,destination,timestamp)) != routeset.end()) return false;
+
+        routequeue.push(make_tuple(source,destination,timestamp));
+        routeset.insert(make_tuple(source,destination,timestamp));
+        routemap.emplace(source, make_pair(destination, timestamp));
+
+        if (routequeue.size() > (size_t)k) {
+            auto old = routequeue.front();
             routequeue.pop();
+            routeset.erase(old);
+
+            int s = get<0>(old);
+            int d = get<1>(old);
+            int t = get<2>(old);
+
+            auto range = routemap.equal_range(s);
+            for (auto it = range.first; it != range.second; ++it) {
+                if (it->second.first == d && it->second.second == t) {
+                    routemap.erase(it);
+                    break;
+                }
+            }
         }
+
         return true;
     }
-    
+
     vector<int> forwardPacket() {
-        if(routequeue.empty())return {};
-        auto [s,d,t]=routequeue.front();
-        routeset.erase(routequeue.front());
-        routemap.erase(s);
+        if (routequeue.empty()) return {};
+        auto [s,d,t] = routequeue.front();
         routequeue.pop();
+
+        routeset.erase(make_tuple(s,d,t));
+
+        auto range = routemap.equal_range(s);
+        for (auto it = range.first; it != range.second; ++it) {
+            if (it->second.first == d && it->second.second == t) {
+                routemap.erase(it);
+                break;
+            }
+        }
+
         return {s,d,t};
     }
-    
+
     int getCount(int destination, int startTime, int endTime) {
-        int count=0;
-        for(auto m: routemap){
-            if(m.second.first==destination && startTime<=m.second.second &&
-           m.second.second<=endTime )count++;
+        int count = 0;
+        for (auto &m : routemap) {
+            int dest = m.second.first;
+            int time = m.second.second;
+            if (dest == destination && startTime <= time && time <= endTime) {
+                count++;
+            }
         }
         return count;
     }
 };
-
-/**
- * Your Router object will be instantiated and called as such:
- * Router* obj = new Router(memoryLimit);
- * bool param_1 = obj->addPacket(source,destination,timestamp);
- * vector<int> param_2 = obj->forwardPacket();
- * int param_3 = obj->getCount(destination,startTime,endTime);
- */

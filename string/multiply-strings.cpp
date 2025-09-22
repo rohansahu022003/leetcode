044cface3e9fab62ab3b233cdbe4b1877d2b8f33
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-22 19:48:01 +0530

    LC-multiply-strings [Runtime: N/A; Memory: N/A]

--- string/multiply-strings.cpp
+++ string/multiply-strings.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        long long a=stoi(num1);
-        long long b=stoi(num2);
+        long long a=stoll(num1);
+        long long b=stoll(num2);
 
         string answer=to_string(a*b);
         return answer;

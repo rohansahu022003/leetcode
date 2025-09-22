5f99683a87f7c9ec7a34fb96c34ab78ea9e02f84
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-22 20:13:41 +0530

    LC-multiply-strings [Runtime: 2 ms; Memory: 9.4 MB]

--- simulation/multiply-strings.cpp
+++ simulation/multiply-strings.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        long long a=stoll(num1);
-        long long b=stoll(num2);
+       
+       int n=num1.size(),m=num2.size();
 
-        string answer=to_string(a*b);
-        return answer;
-    }
+       vector<int>num(n+m,0);
+
+       for(int i=n-1; i>=0;i--){
+        for(int j=m-1; j>=0; j--){
+            int mul=(num1[i]-'0') * (num2[j]-'0');
+            int sum=mul+num[i+j+1];
+
+            num[i+j+1]=sum%10;
+            num[i+j]+=sum/10;
+        }
+
+       }
+       
+       string ans="";
+       for(auto n: num){
+        if(!(ans.empty() && n==0) ){
+            ans.push_back(n+'0');
+        }
+       }
+         return ans.empty()?"0" : ans;
+       }
+      
 };

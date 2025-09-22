398ccad2614d3fdc68f3c15847689d16d710aa93
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-22 19:28:51 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

---
+++ math/powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0)return double(0);
+       else if(x==1)return double(1);
+        if(n==0)return 1;
+        double ans=1;
+
+        if (n>0){
+            for(int i=1; i<=n; i++){
+                ans*=x;
+            }
+        }
+        else {
+            while(n){
+                ans=ans/x;
+               n++;
+            }
+        }
+return ans;
+    }
+};

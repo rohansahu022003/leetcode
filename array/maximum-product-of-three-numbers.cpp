832241fc85dfcc00fad522e911135ac043e22c8f
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 03:16:32 +0530

    LC-maximum-product-of-three-numbers [Runtime: 19 ms; Memory: 31.7 MB]

--- array/maximum-product-of-three-numbers.cpp
+++ array/maximum-product-of-three-numbers.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-int k=3;
-int result=1;
-int i=nums.size()-1;
-        while(k){
-            result*=nums[i];
-            i--;
-            k--;
-
-        }
+        int result=1;
+        int i=nums.size()-1;
+        int result1=result*nums[i]*nums[i-1]*nums[i-2];
+        int result2=result*nums[0]*nums[1]*nums[i];
+        result=max(result1, result2);
         return result;
     }
 };

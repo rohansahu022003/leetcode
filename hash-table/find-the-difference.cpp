a140cf7cd74e71b0dc5a73c4ec3d6c25de59efd9
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 00:28:17 +0530

    LC-find-the-difference [Runtime: 0 ms; Memory: 9.1 MB]

--- hash-table/find-the-difference.cpp
+++ hash-table/find-the-difference.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>f;
-
-        for(int i=0; i<t.size(); i++){
-            f[t[i]]++;
-        }
-
-        for(int i=0; i<s.size(); i++){
-            f[s[i]]--;
-            if(f[s[i]]==0)f.erase(s[i]);
-        }
-      char ans;
-
-      for(auto m: f){
-        ans=m.first;
-      }
-      return ans;
+     int result=0;
+     for(int i=0; i<s.size(); i++){
+        result=result^s[i];
+     }
+     for(int i=0; i<t.size(); i++){
+        result=result^t[i];
+     }
+     return result;
     }
 };

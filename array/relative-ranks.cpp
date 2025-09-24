92be9a19b6832229f97607f45825cc8058ad7a25
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 02:48:51 +0530

    LC-relative-ranks [Runtime: N/A; Memory: N/A]

---
+++ array/relative-ranks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n=score.size();
+        vector<string>ans(n);
+        priority_queue<pair<int,int>>pq;
+
+        for(int i=0; i<score.size(); i++){
+            pq.push({score[i],i});
+        }
+        int k=1;
+        while(!pq.empty()){
+            int i=pq.top().second;
+            if(k==1)ans[i]="Gold Medal";
+            else if(k==2)ans[i]="Silver Medal";
+            else if(k==3)ans[i]="Bronze Medal";
+            else{
+                ans[i]=k+'0';
+            }
+            k++;
+            pq.pop();
+        }
+        return ans;
+    }
+};

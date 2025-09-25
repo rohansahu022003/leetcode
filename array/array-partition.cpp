6d0087a6d859618c21d5c7a28d83a70b605d303d
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 22:26:43 +0530

    LC-array-partition [Runtime: 13 ms; Memory: 32.2 MB]

--- array/array-partition.cpp
+++ array/array-partition.cpp
@@ -3,9 +3,9 @@ public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int result=0;
-        for(int i=0;i<nums.size()-1; i++){
-            result+=min(nums[i],nums[i+1]);
-            i++;
+        for(int i=0;i<nums.size()-1; i+=2){
+            result+=nums[i];
+            
         }
 return result;
     }

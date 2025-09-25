9307a8a5eeff9096de7b8b17efbb70eebf8d7788
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 14:26:43 +0530

    LC-smallest-range-ii [Runtime: 8 ms; Memory: 19.3 MB]

--- math/smallest-range-ii.cpp
+++ math/smallest-range-ii.cpp
@@ -3,17 +3,15 @@ public:
     int smallestRangeII(vector<int>& nums, int k) {
         if(nums.size()==1)return 0;
         sort(nums.begin(), nums.end());
-       int minelement=nums[0];
-       int maxelement=nums[nums.size()-1];
-        int i=0;
-        int j=nums.size()-1;
-        while(i<nums.size() && j>=0){
-            if(nums[j]-k<0)continue;
-            minelement=max(minelement,nums[i++]+k);
-            
-            maxelement=min(maxelement,nums[j--]-k);
+        int n=nums.size();
+        int ans=nums[n-1]-nums[0];
+
+        for(int i=0;i<n-1;i++){
+            int high=max(nums[i]+k,nums[n-1]-k);
+            int low=min(nums[0]+k,nums[i+1]-k);
+            ans=min(ans,high-low);
         }
-        int ans=maxelement-minelement;
         return ans;
+
     }
 };

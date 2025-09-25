867e9cccb909f0da34a8700d2e2c3f195f40d2c3
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 14:47:16 +0530

    LC-sort-array-by-parity-ii [Runtime: N/A; Memory: N/A]

--- sorting/sort-array-by-parity-ii.cpp
+++ sorting/sort-array-by-parity-ii.cpp
@@ -4,8 +4,8 @@ public:
         int i=0;
         int j=nums.size()-1;
         while(i<j){
-            if(i%2==0 && nums[i]%2==0)i++;
-            else if(j%2==1 && nums[j]%2==1)j--;
+            if((i%2==0 && nums[i]%2==0 )|| (i%2==1 &&nums[i]%2==1))i++;
+            else if((j%2==1 && nums[j]%2==1) || (j%2==0 && nums[j]%2==0))j--;
             else{
                 swap(nums[i],nums[j]);
                 i++;

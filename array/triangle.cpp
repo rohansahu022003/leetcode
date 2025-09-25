a261953c5f52fd2616b92139ce6991aba5dc5ddf
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-25 12:36:41 +0530

    LC-triangle [Runtime: N/A; Memory: N/A]

--- array/triangle.cpp
+++ array/triangle.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int result=0;
+        int result=triangle[0][0];
         int n=triangle.size();
+        int l=0;
 
-        for(int i=0; i<n;i++){
-            result+=*min_element(triangle[i].begin(),triangle[i].end());
+        for(int i=1; i<n;i++){
+          auto  minel=min_element(triangle[i].begin()+l,triangle[i].begin()+l+2);
+          result+=*minel;
+          l=distance(triangle[i].begin(), minel);
+         
         }
         return result;
     }

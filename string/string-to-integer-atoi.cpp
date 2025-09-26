2c74666175e5e4aae40ce7fdd8e33ae08fb41915
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-26 23:12:34 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -4,7 +4,9 @@ public:
         long long ans=0;
         for(int i=0; i<s.size();i++){
             if(s[i]==' ')continue;
+            if(i>=1){
             else if(s[i]=='-' && isdigit(s[i-1]))break;
+            }
             else if(s[i]=='-')continue;
             else if(isalpha(s[i]))break;
             else ans=ans*10+s[i]-'0';

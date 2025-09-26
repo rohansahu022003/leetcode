3da42862858b84c1f0965c1adc95bc51e57a9946
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-26 08:42:30 +0530

    LC-valid-triangle-number [Runtime: 31 ms; Memory: 16.7 MB]

--- greedy/valid-triangle-number.cpp
+++ greedy/valid-triangle-number.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
-        if (nums.size()<=2)return 0;
-        int count=0;
-        for(int i=0; i<nums.size()-2; i++){
-            for(int j=i+1; j<nums.size()-1;j++){
-                for(int k=j+1;k<nums.size();k++){
-                    if((nums[i]+nums[j]>nums[k]) && (nums[i]+nums[k]>nums[j]) &&(nums[k]+nums[j]>nums[i])){
-                        count++;
-                    }
-                }
+       sort(nums.begin(), nums.end());
+       int ans=0;
+       for(int i=nums.size()-1; i>=2; i--){
+        int j=0,k=i-1;
+        while(j<k){
+            if(nums[j]+nums[k]>nums[i]){
+                ans+=k-j;
+                k--;
             }
+            else j++;
         }
-        return count;
+       }
+       return ans;
     }
 };

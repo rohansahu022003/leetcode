66288599cbe4271bddde54bdb3b230cf7f1a3157
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-26 23:15:16 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     long long myAtoi(string s) {
         long long ans=0;
+        int count=0;
         for(int i=0; i<s.size();i++){
             if(s[i]==' ')continue;
-            if(i>=1){
-            else if(s[i]=='-' && isdigit(s[i-1]))break;
-            }
+            if(s[i]=='-' && count!=0)break;
             else if(s[i]=='-')continue;
             else if(isalpha(s[i]))break;
-            else ans=ans*10+s[i]-'0';
+            else{
+                count++;
+                 ans=ans*10+s[i]-'0';
+            }
         }
         if(s.find('-')!=string::npos)return -ans;
         return ans;

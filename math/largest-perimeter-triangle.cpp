2202ec693ea8703877aa37fcc315f2be973b10be
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-27 09:46:19 +0530

    LC-largest-perimeter-triangle [Runtime: 23 ms; Memory: 25.6 MB]

--- math/largest-perimeter-triangle.cpp
+++ math/largest-perimeter-triangle.cpp
@@ -2,23 +2,9 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int maxperi=0;
-        int peri=0;
-        
-        for(int i=n-1; i>=2; i--){
-            int j=0;
-            int k=i-1;
-            while(j<k){
-            if(nums[j]+nums[k]>nums[i]){
-                peri=nums[j]+nums[k]+nums[i];
-                maxperi=max(peri,maxperi);
-                j++;
-            }
-            else j++;
-            }
-
+        for(int i =nums.size()-1 ; i > 1; i--){
+            if(nums[i-2]+nums[i-1]>nums[i]) return nums[i]+nums[i-1]+nums[i-2];
         }
-        return maxperi;
+        return 0;
     }
 };

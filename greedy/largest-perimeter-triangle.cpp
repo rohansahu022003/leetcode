156644ec031ff4e4ffeca00216fb09ba34f772a0
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-27 09:45:22 +0530

    LC-largest-perimeter-triangle [Runtime: 1268 ms; Memory: 25.6 MB]

--- greedy/largest-perimeter-triangle.cpp
+++ greedy/largest-perimeter-triangle.cpp
@@ -5,8 +5,9 @@ public:
         int n=nums.size();
         int maxperi=0;
         int peri=0;
-        int j=0;
+        
         for(int i=n-1; i>=2; i--){
+            int j=0;
             int k=i-1;
             while(j<k){
             if(nums[j]+nums[k]>nums[i]){

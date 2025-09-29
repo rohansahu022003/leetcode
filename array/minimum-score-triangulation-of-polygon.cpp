64933e80134c2d8b8aea4b441892d2c6454962a1
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-09-30 00:50:05 +0530

    LC-minimum-score-triangulation-of-polygon [Runtime: 0 ms; Memory: 11.2 MB]

--- array/minimum-score-triangulation-of-polygon.cpp
+++ array/minimum-score-triangulation-of-polygon.cpp
@@ -1,14 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int minScoreTriangulation(vector<int>& values) {
-        sort(values.begin(),values.end());
-        int ans=0;
-        
-        for(int i=2; i<values.size(); i++){
-            int mul=values[0]*values[1];
-            mul*=values[i];
-            ans+=mul;
-        }
-        return ans;
+    int minScoreTriangulation(vector<int>& v) {
+        int n=v.size(); vector<vector<int>> dp(n,vector<int>(n));
+        for(int l=3;l<=n;l++)
+            for(int i=0;i+l-1<n;i++){
+                int j=i+l-1; dp[i][j]=1e9;
+                for(int k=i+1;k<j;k++)
+                    dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]+v[i]*v[j]*v[k]);
+            }
+        return dp[0][n-1];
     }
 };

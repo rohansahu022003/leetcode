75e4c1e4770275934b540db36be77b206bc1783d
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-10-08 20:58:24 +0530

    LC-successful-pairs-of-spells-and-potions [Runtime: 4 ms; Memory: 125.4 MB]

--- two-pointers/successful-pairs-of-spells-and-potions.cpp
+++ two-pointers/successful-pairs-of-spells-and-potions.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        sort(potions.begin(),potions.end());
-           int n=spells.size();
-           int j=potions.size()-1;
-           vector<int>ans(n,0);
-        for(int i=0; i<spells.size(); i++){
-            for(int k=0; k<=j; k++){
-                if(spells[i]*potions[k]>=success){
-                    ans[i]=j-k+1;
-                    break;
-                }
-                else if(spells[i]*potions[j]<success){
-                    break;
-
-                }
-            }
-
+    static vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int freq[100001]={0}, pMax=0;
+        for(int p: potions){
+            freq[p]++;
+            pMax=max(pMax, p);
         }
-        return ans;
+      
+        partial_sum( freq, freq+pMax+1, freq);
+
+        const int n=spells.size(), m=potions.size();
+        vector<int> result(n, 0);
+        
+        for (int i=0; i<n; i++) {
+            const int spell=spells[i];
+            const long long k = (success+spell-1)/spell;
+            if (k<=pMax) {
+                result[i]=m-(k>=1?freq[k-1]:0);
+            }
+        }       
+        return result;
     }
 };

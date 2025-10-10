29875cd67f880880358422d4ff4c194212f0ae56
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-10-10 23:55:16 +0530

    LC-taking-maximum-energy-from-the-mystic-dungeon [Runtime: 74 ms; Memory: 152.6 MB]

--- array/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ array/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         int n=energy.size();
         vector<int>dp(n,0);
         int result=INT_MIN;

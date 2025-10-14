1fd0ae89841bc1ce1506ed7ae68303976ae729ea
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-10-14 17:20:43 +0530

    LC-adjacent-increasing-subarrays-detection-i [Runtime: N/A; Memory: N/A]

---
+++ array/adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int i=0;
+        int j=k;
+        int sub1=0;
+        int sub2=0;
+        int ind1=i;
+        int ind2=j;
+        while(j+k<nums.size()){
+            if(nums[ind1]<nums[ind1+1] && sub1<2){
+                sub1++;
+                ind1++;
+            }
+            else if(nums[ind2]<nums[ind2+1] && sub1==2 && sub2<2){
+                sub2++;
+                ind2++;
+            }
+            else if(sub1==2 && sub2==2)return true;
+
+            else{
+                i++;
+                j++;
+                ind1=i;
+                ind2=j;
+                sub1=0;
+                sub2=0;
+            }
+
+
+        }
+        return false;
+    }
+};

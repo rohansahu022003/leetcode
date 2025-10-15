8ae76cbdf9e22e812700a1e2a981b6c1bce54ee8
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-10-15 20:32:41 +0530

    LC-adjacent-increasing-subarrays-detection-ii [Runtime: N/A; Memory: N/A]

---
+++ binary-search/adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int prev=1;
+        int curr=1;
+        int k=0;
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i+1]>nums[i]){
+                curr++;
+                k=max(k,curr/2);
+            }
+            else {
+                k=max(k,min(prev,curr));
+                prev=curr;
+                curr=1;
+            }
+
+        }
+
+      
+        return k;
+    }
+};

fbee65eac26f9f62f439f0c16ff07296b7ec42fb
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-03 00:08:25 +0530

    LC-count-unguarded-cells-in-the-grid [Runtime: 808 ms; Memory: 277.6 MB]

---
+++ matrix/count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    private:
+    void dfs(int r, int c, string C,vector<vector<int>>& vis, 
+     map<pair<int,int>,int> & mp  ){
+        int m=vis.size();
+        int n=vis[0].size();
+        if(r<0 ||c<0 || r>=m || c>=n)return;
+        else if(mp.find({r,c})!=mp.end())return;
+        vis[r][c]=1;
+
+        if(C=="l")dfs(r,c-1,"l",vis,mp);
+        else if(C=="r") dfs(r,c+1,"r",vis,mp);
+        else if(C=="d")  dfs(r+1,c,"d",vis,mp);
+        else dfs(r-1,c,"u",vis,mp);
+        
+     }
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+      vector<vector<int>>vis(m,vector<int>(n));
+      map<pair<int,int>,int>mp;
+
+      for(auto it:guards){
+        mp[{it[0],it[1]}]++;
+        vis[it[0]][it[1]]=1;
+      }
+      for(auto w:walls){
+        mp[{w[0],w[1]}]++;
+        vis[w[0]][w[1]]=1;
+      }
+
+      for(auto it: guards){
+        int r=it[0];
+        int c=it[1];
+        dfs(r,c-1,"l",vis,mp);
+        dfs(r,c+1,"r",vis,mp);
+        dfs(r+1,c,"d",vis,mp);
+        dfs(r-1,c,"u",vis,mp);
+      }
+
+      int cnt=0;
+      for(int i=0;i<m; i++){
+        for(int j=0; j<n; j++){
+            if(vis[i][j]==0)cnt++;
+        }
+      }
+
+      return cnt;
+
+    }
+};

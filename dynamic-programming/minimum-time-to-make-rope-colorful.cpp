3b47d2de4bed6081780cfcfeb0dc7d91f5648c68
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-03 08:23:52 +0530

    LC-minimum-time-to-make-rope-colorful [Runtime: 4 ms; Memory: 99.9 MB]

--- dynamic-programming/minimum-time-to-make-rope-colorful.cpp
+++ dynamic-programming/minimum-time-to-make-rope-colorful.cpp
@@ -2,10 +2,21 @@ class Solution {
 public:
     int minCost(string colors, vector<int>& n) {
         int ans=0;
-        for(int i=0; i<colors.size()-1; i++){
-            if(colors[i]==colors[i+1]){
-                ans+=min(n[i],n[i+1]);
+        int i=0;
+        int j=1;
+        while(j<colors.size()){
+            while(colors[i]=='0')i++;
+            if(colors[i]==colors[j]){
+                ans+=min(n[i],n[j]);
+                if(n[i]<n[j])colors[i]='0';
+                else colors[j]='0';
+                j++;
 
+            }
+            
+            else {
+                i++;
+                j++;
             }
         }
         return ans;

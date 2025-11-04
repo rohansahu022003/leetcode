45a682d23cec375e89a6bbe803f76817a17b3da2
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-04 22:13:40 +0530

    LC-find-x-sum-of-all-k-long-subarrays-i [Runtime: N/A; Memory: N/A]

---
+++ hash-table/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        vector<int>ans;
+        int n=nums.size();
+        for(int i=0; i<n-k+1; i++){
+            unordered_map<int,int>freq;
+            priority_queue<pair<int,int>>pq;
+            int sum=0;
+            vector<int>temp(nums.begin()+i, nums.begin()+k+i);
+            for(auto t:temp)freq[t]++;
+            for(auto f:freq)pq.push({f.second,f.first});
+            for(int j=0; j<x; j++){
+                auto[fr,nu]=pq.top();
+                pq.pop();
+                sum+=fr*nu;
+            }
+            ans.push_back(sum);
+
+        }
+        return ans;
+    }
+};

506df36906e802f2ac140950be7da7a414bdff7f
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-06 00:39:33 +0530

    LC-minimum-operations-to-make-median-of-array-equal-to-k [Runtime: N/A; Memory: N/A]

--- sorting/minimum-operations-to-make-median-of-array-equal-to-k.cpp
+++ sorting/minimum-operations-to-make-median-of-array-equal-to-k.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     long long minOperationsToMakeMedianK(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-int count=0;
+long long count=0;
         if(nums.size()%2==1){
             long long mid= ceil(nums.size()/2);
             if(nums[mid]!=k)count+=abs(nums[mid]-k);
-            if(nums[mid-1]>k)count+=abs(nums[mid-1]-k);
-            if(nums[mid+1]<k)count+=abs(nums[mid+1]-k);
+            if(mid-1>=0 && nums[mid-1]>k)count+=abs(nums[mid-1]-k);
+            if(mid+1<nums.size() && nums[mid+1]<k)count+=abs(nums[mid+1]-k);
         }
        
 

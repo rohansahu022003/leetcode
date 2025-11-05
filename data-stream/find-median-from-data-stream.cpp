52ecf25c23b7eb689abc065b1bb257a4708db1b7
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-05 20:54:12 +0530

    LC-find-median-from-data-stream [Runtime: N/A; Memory: N/A]

---
+++ data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,22 @@
+class MedianFinder {
+    vector<double>arr;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        arr.push_back(num);
+    }
+    
+    double findMedian() {
+        double ans;
+        if(arr.size()%2==0){
+            ans=(arr[arr.size()/2]+arr[(arr.size()/2)-1])/2;
+        }
+        else{
+            ans=arr[arr.size()/2];
+        }
+        return ans;
+    }
+};

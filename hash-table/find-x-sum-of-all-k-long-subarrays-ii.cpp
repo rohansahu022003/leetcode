e0f35f6901e88dfa7c91405b1dda9c01c0bb6837
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-05 22:33:15 +0530

    LC-find-x-sum-of-all-k-long-subarrays-ii [Runtime: 935 ms; Memory: 286.3 MB]

--- hash-table/find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ hash-table/find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -1,25 +1,80 @@
+
+long long result;
+set<pair<int, int>> small;
+set<pair<int, int>> large;
+map<int, int> freq;
+
+void insertIntoSet(pair<int, int> p, int x) {
+    if (large.size() < x || *(large.begin()) < p) {
+
+        result += p.first * 1LL * p.second;
+        large.insert(p);
+
+        if (large.size() > x) {
+
+            auto it = *large.begin();
+            result -= it.first * 1LL * it.second;
+            large.erase(it);
+            small.insert(it);
+        }
+
+    } else {
+        small.insert(p);
+    }
+}
+
+void removeFromSet(pair<int, int> p) {
+
+    if (p >= *(large.begin())) {
+        result -= p.first * 1LL * p.second;
+        large.erase(p);
+
+        if (small.size() > 0) {
+            auto it = *small.rbegin();
+            result += it.first * 1LL * it.second;
+            small.erase(it);
+            large.insert(it);
+        }
+
+    } else {
+        small.erase(p);
+    }
+}
+
+void insert(int num, int x) {
+    if (freq[num]) {
+        removeFromSet({freq[num], num});
+    }
+    freq[num]++;
+    insertIntoSet({freq[num], num}, x);
+}
+
+void remove(int num, int x) {
+    removeFromSet({freq[num], num});
+    freq[num]--;
+    if (freq[num]) {
+        insertIntoSet({freq[num], num}, x);
+    }
+}
+
 class Solution {
 public:
     vector<long long> findXSum(vector<int>& nums, int k, int x) {
-        vector<long long>ans;
-        int n=nums.size();
-        for(int i=0; i<n-k+1; i++){
-            unordered_map<long long,long long>freq;
-            priority_queue<pair<long long,long long>>pq;
-            long long sum=0;
-            int j=0;
-            vector<int>temp(nums.begin()+i, nums.begin()+k+i);
-            for(auto t:temp)freq[t]++;
-            for(auto f:freq)pq.push({f.second,f.first});
-            while(j<x && !pq.empty()){
-                auto[fr,nu]=pq.top();
-                pq.pop();
-                sum+=fr*nu;
-                j++;
-            }
-            ans.push_back(sum);
+        result = 0;
+        small.clear();
+        large.clear();
+        freq.clear();
 
+        vector<long long> ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            insert(nums[i], x);
+            if (i >= k) {
+                remove(nums[i - k], x);
+            }
+            if (i >= k - 1) {
+                ans.push_back(result);
+            }
         }
-        return ans; 
+        return ans;
     }
 };

d50d58aac80ec72457a762db9572e2c22be9fe72
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-05 21:13:50 +0530

    LC-find-median-from-data-stream [Runtime: 2603 ms; Memory: 163 MB]

--- sorting/find-median-from-data-stream.cpp
+++ sorting/find-median-from-data-stream.cpp
@@ -1,47 +1,27 @@
 class MedianFinder {
+    multiset<double>arr;
 public:
-    priority_queue<int,vector<int>>maxHeap;
-    priority_queue<int,vector<int>,greater<int>>minHeap;
-    
     MedianFinder() {
-       
+        
     }
     
     void addNum(int num) {
-        if(maxHeap.empty() or num<=maxHeap.top()){
-            maxHeap.push(num);
-        }else{
-            minHeap.push(num);
-        }
-
-        //now try to balance both the heaps with size difference being atmost 1:
-        int a = maxHeap.size() , b = minHeap.size();
+        arr.insert(num);
         
-        if(a>b and a-b > 1){
-            int val = maxHeap.top();maxHeap.pop();
-            minHeap.push(val);
-        }else if(b>a and b-a >= 1){
-            int val = minHeap.top();
-            minHeap.pop();
-            maxHeap.push(val);
-        }
-
     }
     
     double findMedian() {
-        int a = maxHeap.size() , b = minHeap.size();
+        double ans;
+        int n=arr.size();
+        auto it=arr.begin();
 
-        double ans = 0;
-        if(a==b){
-            //this is the even case:
-            int val1 = maxHeap.top();
-            int val2 = minHeap.top();
-
-            ans = (val1 + val2)/2.0;
-        }else{
-            ans = maxHeap.top();
+        advance(it,n/2);
+        if(n%2==0){
+            auto it2=it;
+            it2--;
+            ans=(*it2+*it)/2.0;
         }
-
+        else ans= *it;
         return ans;
     }
 };

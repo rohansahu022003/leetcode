30731a9910158aaface7c2784c93fc1692981bf7
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-06 19:09:16 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: 3 ms; Memory: 97.2 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        int minprice=INT_MAX;
-        for(auto price: prices){
-            minprice=min(minprice, price);
-            profit=max(profit, price-minprice);
-        }
-        return profit;
+       int profit=0;
+       int minprice=INT_MAX;
+
+       for(auto p:prices){
+        minprice=min(minprice,p);
+        profit=max(profit,p-minprice);
+       } 
+       return profit;
     }
 };

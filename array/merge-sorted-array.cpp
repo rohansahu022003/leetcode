1e82c96885b93fe08629666aef65df77b21f9fed
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-06 21:38:23 +0530

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 12.4 MB]

--- array/merge-sorted-array.cpp
+++ array/merge-sorted-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(nums2.size()==0)return;
-        if(nums1.size()==1 && nums2.size()==1)nums1=nums2;
-        int j=0;
-        for(int i=nums1.size()-1; i>=0; i--){
-            if(nums1[i]==0 && j<nums2.size())nums1[i]=nums2[j++];
-            else {
-                break;
-            }
-        }
-        sort(nums1.begin(), nums1.end());
+       if(nums2.size()==0)return;
+       if(nums1.size()==1)nums1=nums2;
+    int j=nums2.size()-1;
+       for(int i=nums1.size()-1; i>=m; i--){
+        nums1[i]=nums2[j];
+       if(j>=0) j--;
+       }
+       sort(nums1.begin(), nums1.end());
 
     }
 };

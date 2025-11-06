1fda600e5b20054323b1bc0397388e706e372389
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-07 00:03:00 +0530

    LC-majority-element [Runtime: 3 ms; Memory: 28.2 MB]

--- array/majority-element.cpp
+++ array/majority-element.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ele;
-        int cnt=0;
-        for(int i=0; i<nums.size(); i++){
-            if(cnt==0){
-                cnt++;
-                ele=nums[i];
-            }
-            else if(nums[i]==ele){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
-        }
-        int cnt2;
-        for(int i=0; i<nums.size(); i++){
-            if(ele==nums[i]){
-                cnt2++;
-            }
-            if(cnt2>nums.size()/2){
-                return ele;
-            }
-        }
-        return -1;
+       unordered_map<int,int>freq;
+
+       for(int i=0; i<nums.size(); i++){
+        freq[nums[i]]++;
+       } 
+int ans;
+       for(auto M: freq){
+        if(M.second>nums.size()/2)ans=M.first;
+       }
+
+       return ans;
     }
 };

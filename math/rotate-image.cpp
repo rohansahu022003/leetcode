c4bc316652965eb8903d7cd49311a617b194f92b
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-06 20:36:16 +0530

    LC-rotate-image [Runtime: 0 ms; Memory: 10.2 MB]

--- math/rotate-image.cpp
+++ math/rotate-image.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-       int row=matrix.size();
-       int col= row>0?matrix[0].size():0;
-       for(int i=0; i<row-1; i++){
-        for(int j=i+1; j<col; j++){
-            swap(matrix[i][j],matrix[j][i]);
+        int row=matrix.size();
+        int col=row>0?matrix[0].size():0;
+
+        for(int i=0; i<row-1; i++){
+            for(int j=i+1; j<col; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+
         }
 
-       }
-       for(int i=0; i<row; i++){
-        reverse(matrix[i].begin(), matrix[i].end());
-       }
+        for(int i=0; i<row; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };

f32470d922a521f06f082bbaa166df53404105c9
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-07 19:53:46 +0530

    LC-top-k-frequent-elements [Runtime: 7 ms; Memory: 17.8 MB]

--- quickselect/top-k-frequent-elements.cpp
+++ quickselect/top-k-frequent-elements.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>freq;
-        using T=pair<int,int>;
-        priority_queue<T>pq;
-        for(int i=0; i<nums.size(); i++){
-            freq[nums[i]]++;
-        }
+      unordered_map<int,int>freq;
 
-        for(auto f : freq){
-            pq.push({f.second,f.first});
-        }
-   vector<int>ans;
-        while(k){
-            int i=pq.top().second;
-   ans.push_back(i);
-      pq.pop();
-      k--;
-        }
-     return ans;
+      for(auto num : nums){
+        freq[num]++;
+      }
+      priority_queue<pair<int,int>>pq;
+      for(auto f: freq){
+        pq.push({f.second,f.first});
+      }
+vector<int>ans;
+      while(k--){
+        ans.push_back(pq.top().second);
+        pq.pop();
+      }
+      return ans;
     }
 };

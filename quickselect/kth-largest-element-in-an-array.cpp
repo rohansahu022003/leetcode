158bc01996f10f15dabcec0f61791587fd92e4b5
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-07 18:48:59 +0530

    LC-kth-largest-element-in-an-array [Runtime: 23 ms; Memory: 64.7 MB]

--- quickselect/kth-largest-element-in-an-array.cpp
+++ quickselect/kth-largest-element-in-an-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-      priority_queue<int>pq;
-      for(int i=0; i<nums.size();i++){
-        pq.push(nums[i]);
-      }  
-    int i;
-      while(k){
-        i=pq.top();
-        pq.pop();
-        k--;
-      }
-      return i;
+    priority_queue<int>pq;
+
+    for(auto num: nums){
+        pq.push(num);
+    }int ans;
+    while(k){
+ans=pq.top();
+pq.pop();
+k--;
+    }
+    return ans;
     }
 };

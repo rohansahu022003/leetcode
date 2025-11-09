3f3f90ef2925f601545182c87be299c45cca7b7a
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-10 02:56:44 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- greedy/jump-game-ii.cpp
+++ greedy/jump-game-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        if(nums.size()==1)return 0;
         int ans=0;
         int remain=1;
         for(int i=0; i<nums.size(); i++){

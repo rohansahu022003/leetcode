b73282dd48c6276e6acd9fac0b78e69489924eb0
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-09 09:32:58 +0530

    LC-count-operations-to-obtain-zero [Runtime: 0 ms; Memory: 7.9 MB]

--- simulation/count-operations-to-obtain-zero.cpp
+++ simulation/count-operations-to-obtain-zero.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countOperations(int num1, int num2) {
         int k=0;
-        while(num1 && num2){
+        while(num1!=0&& num2!=0){
             if(num1>=num2)num1-=num2;
             else num2-=num1;
             k++;

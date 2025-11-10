ee5a8b3be5d301dff343160346c0f5ebae2de61e
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-10 14:30:29 +0530

    LC-minimum-operations-to-convert-all-elements-to-zero [Runtime: 49 ms; Memory: 222.1 MB]

--- stack/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ stack/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
        int ans=0;
-       vector<int>stack;
+       stack<int>stack;
 
        for(auto num : nums){
-        while(!stack.empty() && stack.back()>num){
-            stack.pop_back();
+        while(!stack.empty() && stack.top()>num){
+            stack.pop();
         }
         if(num==0)continue;
 
-        if(stack.empty() || stack.back()<num){
+        if(stack.empty() || stack.top()<num){
             ans++;
-            stack.push_back(num);
+            stack.push(num);
         }
        }
        return ans;

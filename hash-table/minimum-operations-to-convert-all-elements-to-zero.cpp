5514fc40c385aae69ce67601e0f4c4d7e0b8b624
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-10 14:29:42 +0530

    LC-minimum-operations-to-convert-all-elements-to-zero [Runtime: 3 ms; Memory: 222 MB]

--- hash-table/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ hash-table/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        unordered_map<int,int>freq;
+       int ans=0;
+       vector<int>stack;
 
-        for(int i=0; i<nums.size();i++){
-           if(nums[i]!=0) freq[nums[i]]++;
+       for(auto num : nums){
+        while(!stack.empty() && stack.back()>num){
+            stack.pop_back();
         }
-        int maxel=*max_element(nums.begin(),nums.end());
-if(freq.size()==1)return 1;
-        int ans=0;
-        for(auto f:freq){
-            if(f.first==maxel)ans+=f.second;
-            else ans++;
-        }
+        if(num==0)continue;
 
-        return ans;
+        if(stack.empty() || stack.back()<num){
+            ans++;
+            stack.push_back(num);
+        }
+       }
+       return ans;
     }
 };

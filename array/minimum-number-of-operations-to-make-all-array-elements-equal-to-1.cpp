47a7a51809f68b9fb64094ccc7361e12e0643019
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-12 06:40:06 +0530

    LC-minimum-number-of-operations-to-make-all-array-elements-equal-to-1 [Runtime: N/A; Memory: N/A]

--- array/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ array/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int num1 = 0, overall_g = 0;
+        int n=nums.size();
+       int num0=0,overall=0;
+       for(auto num: nums){
+        if(num==1)num0++;
+        overall=gcd(overall,num);
+       }
+       if(num0>0)return n-num0;
+       else if(overall>1)return -1;
 
-        for (int x : nums) {
-            if (x == 1) num1++;
-            overall_g = gcd(overall_g, x);
-        }
-
-        if (num1 > 0) return n - num1;
-        if (overall_g > 1) return -1;
-
-        int min_len = n;
-        for (int i = 0; i < n; i++) {
-            int g = nums[i];
-            if (g == 1) {
-                min_len = 1;
-                break; // minimal possible
-            }
-            for (int j = i + 1; j < n; j++) {
-                g = gcd(g, nums[j]);
-                if (g == 1) {
-                    min_len = min(min_len, j - i + 1);
-                    break;
-                }
+       int mingcd=n;
+       for(int i=0; i<n-1; i++){
+        int g=nums[i];
+        for(int j=i+1; j<n; j++){
+            g=gcd(g,nums[j]);
+            if(g==1){
+                mingcd=min(mingcd,j-i);
+                break;
             }
         }
-
-        return min_len + n - 2;
+       }
+       return n-mingcd+1;
     }
 };

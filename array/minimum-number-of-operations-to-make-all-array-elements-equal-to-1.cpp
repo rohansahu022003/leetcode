6dec6bb117554f7e1c01ad7bae5c6d32f14196d5
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-12 06:32:25 +0530

    LC-minimum-number-of-operations-to-make-all-array-elements-equal-to-1 [Runtime: 0 ms; Memory: 31.8 MB]

--- array/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ array/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        if(find(nums.begin(),nums.end(),1)!=nums.end())return nums.size()-1;
-        for(int i=0; i<nums.size()-1; i++){
-            if(gcd(nums[i], nums[i+1])==1)return nums.size();
+        int n = nums.size();
+        int num1 = 0, overall_g = 0;
+
+        for (int x : nums) {
+            if (x == 1) num1++;
+            overall_g = gcd(overall_g, x);
         }
-        return -1;
+
+        if (num1 > 0) return n - num1;
+        if (overall_g > 1) return -1;
+
+        int min_len = n;
+        for (int i = 0; i < n; i++) {
+            int g = nums[i];
+            if (g == 1) {
+                min_len = 1;
+                break; // minimal possible
+            }
+            for (int j = i + 1; j < n; j++) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    min_len = min(min_len, j - i + 1);
+                    break;
+                }
+            }
+        }
+
+        return min_len + n - 2;
     }
 };

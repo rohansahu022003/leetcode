f7882a1228a999ec5e37166397ab09fd8433ccc5
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-12 09:04:43 +0530

    LC-base-7 [Runtime: N/A; Memory: N/A]

---
+++ string/base-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        int n=abs(num);
+string ans="";
+        while(n){
+            int d=n%7;
+            n/=7;
+            ans.push_back('0'+d);
+        }
+
+        if(num<0)ans.push_back('-');
+        reverse(ans.begin(),ans.end());
+        return ans;
+
+    }
+};

1755a422d0553a0f343b74ff1ba8bbf801851cde
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-12 08:58:17 +0530

    LC-construct-the-rectangle [Runtime: N/A; Memory: N/A]

--- math/construct-the-rectangle.cpp
+++ math/construct-the-rectangle.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-      int l;
-      int w;
-int dist=INT_MAX;
-      for(int i=1; i<=ceil(area/2.0); i++){
-        if(area%i==0){
-           int j=area/i;
-            if(abs(j-i)<dist){
-                l=j;
-                w=i;
-                dist=j-i;
-            }
-        }
-      }  
-      return {l,w};
-    }
+     int w=sqrt(area);
+
+     while(area%w!=0){
+        w--;
+     }
+     return {area/w,w};
 };

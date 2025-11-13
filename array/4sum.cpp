58a1575219beff26de0a083299b28833cb37e875
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-13 17:57:12 +0530

    LC-4sum [Runtime: 19 ms; Memory: 17.4 MB]

--- array/4sum.cpp
+++ array/4sum.cpp
@@ -2,32 +2,39 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>>ans;
-        set<pair<int,pair<int,pair<int,int>>>>st;
+        vector<vector<int>> ans;
+        int n = nums.size();
 
-        int n=nums.size();
-        for(int i=0; i<n-3; i++){
-            for(int j=i+1;j<n-2;j++){
-                int k=j+1;
-                int m=n-1;
-                while(k<m){
-                int a=nums[i];
-                int b=nums[j];
-                int c=nums[k];
-                int d=nums[m];
-                int sum=a+b+c+d;
+        for(int i = 0; i < n - 3; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue; 
 
-                if(sum==target && st.find({a,{b,{c,d}}})==st.end()){
-                    st.insert({a,{b,{c,d}}});
-                    ans.push_back({a,b,c,d});
-                    k++;
-                    m--;
-                }
-                else if(sum<target)k++;
-                else m--;
+            for(int j = i + 1; j < n - 2; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = j + 1;
+                int m = n - 1;
+
+                while(k < m) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[m];
+
+                    if(sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[m]});
+
+                        k++;
+                        m--;
+
+                      
+                        while(k < m && nums[k] == nums[k - 1]) k++;
+
+                     
+                        while(k < m && nums[m] == nums[m + 1]) m--;
+                    }
+                    else if(sum < target) k++;
+                    else m--;
                 }
             }
         }
+
         return ans;
     }
 };

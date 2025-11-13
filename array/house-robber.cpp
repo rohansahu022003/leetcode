a35c2cedc919b2b7c538ce0a9c0c31cd4085f2cb
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-13 12:54:00 +0530

    LC-house-robber [Runtime: 0 ms; Memory: 10.6 MB]

--- array/house-robber.cpp
+++ array/house-robber.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        for(int i=2; i<nums.size();i++){
-            nums[i]+=*max_element(nums.begin(), nums.begin()+(i-1));
+        int n=nums.size();
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[0],nums[1]);
 
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2 ;i<n; i++){
+            dp[i]=dp[i]+max(dp[i-1],nums[i]+dp[i-2]);
         }
-        int ans=*max_element(nums.begin(),nums.end());
+        int ans=dp[n-1];
         return ans;
     }
 };

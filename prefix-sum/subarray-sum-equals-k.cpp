6c7c049feb1eb97e2bb2530d7ef82da6c0f34ea3
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-13 23:44:37 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- prefix-sum/subarray-sum-equals-k.cpp
+++ prefix-sum/subarray-sum-equals-k.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-       int sum=0,cnt=0;
-       map<int,int>presum;
-       presum[0]=1;
-       int n=nums.size();
-
-       for(int i=0; i<n; i++){
-        sum+=nums[i];
-        int remove=sum-k;
-        cnt+=presum[remove];
-        presum[sum]+=1;
-       }
-       return cnt;
+        sort(nums.begin(), nums.end());
+        int i=0;
+        if(numd[0]==k)count++;
+        int j=1;
+        
+        while(j<nums.size()){
+           
+        }
+        
+        return count;
     }
-
 };

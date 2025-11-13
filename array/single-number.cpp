17f6b9fff17f52b1c8d661d00eecd2cdb1d6f172
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-13 23:35:37 +0530

    LC-single-number [Runtime: 0 ms; Memory: 20.6 MB]

--- array/single-number.cpp
+++ array/single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1=0;
+        int ans=0;
         for(int i=0; i<nums.size(); i++){
-            xor1^=nums[i];
+            ans^=nums[i];
         }
-        return xor1;
+        return ans;
     }
 };

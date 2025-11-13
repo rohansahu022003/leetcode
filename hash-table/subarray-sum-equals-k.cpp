be0edebf3b4e87f38366fae71680cd11de61ae4d
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-13 23:49:46 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- hash-table/subarray-sum-equals-k.cpp
+++ hash-table/subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
+       
         int i=0;
         int count=0;
         int j=0;

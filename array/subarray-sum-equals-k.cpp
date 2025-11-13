71326bb8cb55ce3d234a0f2f404b7dd5c3cf0ac9
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-13 23:48:53 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- array/subarray-sum-equals-k.cpp
+++ array/subarray-sum-equals-k.cpp
@@ -3,11 +3,25 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int i=0;
-        if(numd[0]==k)count++;
-        int j=1;
+        int count=0;
+        int j=0;
         
         while(j<nums.size()){
-           
+            int sum;
+           if(i==j)sum=nums[i];
+           else if(i!=j)sum=nums[i]+nums[j];
+
+           if(sum==k){
+            count++;
+            i++;
+            j++;
+           }
+           else if(sum>k){
+            i++;
+           }
+           else{
+            j++;
+           }
         }
         
         return count;

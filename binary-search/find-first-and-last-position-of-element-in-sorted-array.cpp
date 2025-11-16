f461fd800aa5187a26dfa81746f5e95d8bade5ac
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-17 04:00:13 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: 0 ms; Memory: 17.4 MB]

---
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int a=-1, b=-1;
+        int low=0;
+        int high=nums.size()-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+               a=mid;
+            }
+             if(nums[mid]<target)low=mid+1;
+            else high=mid-1;
+        }
+
+         low=0;
+         high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)b=mid;
+            if(nums[mid]<=target)low=mid+1;
+            else high=mid-1;
+        }
+        return {a,b};
+
+    }
+};

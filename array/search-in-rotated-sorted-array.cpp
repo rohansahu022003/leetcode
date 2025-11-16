24fd99c75e22df4a9979babe2e7f6db59fd29c27
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-17 03:36:18 +0530

    LC-search-in-rotated-sorted-array [Runtime: 0 ms; Memory: 15.1 MB]

--- array/search-in-rotated-sorted-array.cpp
+++ array/search-in-rotated-sorted-array.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size()-1;
-
-        while(low<=high){
-            int mid=low+(high-low)/2;
-
-            if(nums[mid]==target)return mid;
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && nums[mid]>=target)high=mid-1;
-                else{
-                    low=mid+1;
-                }
-            }
-            else{
-                if(nums[mid]<=target && nums[high]>=target)low=mid+1;
-                else high=mid-1;
-            }
-        }
+       for(int i=0; i<nums.size(); i++){
+        if(nums[i]==target)return i;
+       }
         return -1;
     }
 };

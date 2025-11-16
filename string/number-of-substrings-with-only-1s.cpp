4847af711a8a5368d2457d9127dbc18399638425
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-17 01:16:27 +0530

    LC-number-of-substrings-with-only-1s [Runtime: 0 ms; Memory: 11.5 MB]

--- string/number-of-substrings-with-only-1s.cpp
+++ string/number-of-substrings-with-only-1s.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int numSub(string s) {
         int count=0;
-int ans=0;
+long long ans=0;
+int const mod=7+1e9;
         for(auto c:s){
             if(c=='0'){
                 while(count){
-                 ans+=count;
+                 ans= (ans+count)%mod;
                  count--;
                 }
             }

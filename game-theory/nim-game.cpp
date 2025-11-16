fa0515b83c7eb62067c746c6cb80618cbcc67712
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-17 04:39:05 +0530

    LC-nim-game [Runtime: N/A; Memory: N/A]

--- game-theory/nim-game.cpp
+++ game-theory/nim-game.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-        int low=n/3;
-        int high=ceil(n/3.0);
-        if(low%2==1 & high%2==0)return false;
-        return true;
+        if(n>=3)return true;
+       if(n%3==1)return false;
+       return true;
     }
 };

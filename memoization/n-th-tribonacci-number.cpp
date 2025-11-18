24d2496a96223cbd1588fa34f26ef51c684ef655
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-19 01:08:33 +0530

    LC-n-th-tribonacci-number [Runtime: 0 ms; Memory: 8.4 MB]

--- memoization/n-th-tribonacci-number.cpp
+++ memoization/n-th-tribonacci-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int tribonacci(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        
         vector<int>dp(n+1);
         dp[0]=0;
         dp[1]=1;

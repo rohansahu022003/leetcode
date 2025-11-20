182b7e1ce401e1b0e90cb30adcf39012294b2f0b
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-21 01:39:47 +0530

    LC-maximum-subarray [Runtime: 0 ms; Memory: 74.7 MB]

--- array/maximum-subarray.cpp
+++ array/maximum-subarray.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=0;
-        int ans=INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-              sum+=nums[i];
-              ans=max(sum,ans);
-              if (sum<0)sum=0;
-                    }
-                    return ans;
+       int ans=nums[0];
+int n=nums.size();
+       vector<int>dp(n,0);
+       dp[0]=nums[0];
+       for(int i=1; i<n; i++){
+        dp[i]=nums[i]+(dp[i-1]>0?dp[i-1]:0);
+        ans=max(ans,dp[i]);
+       }
+       return ans;
     }
 
 };

f0786bdc77abd08481ad2297378497be5b7e965a
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-11-23 17:00:12 +0530

    LC-first-missing-positive [Runtime: 10 ms; Memory: 55 MB]

--- hash-table/first-missing-positive.cpp
+++ hash-table/first-missing-positive.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int ans=1;
         for(int i=0; i<nums.size(); i++){
             if(nums[i]==ans)ans++;

c59dbf326dc95b00848b9798f11f323c78b580a5
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-25 22:26:23 +0530

    LC-maximum-gap [Runtime: 50 ms; Memory: 85.8 MB]

--- radix-sort/maximum-gap.cpp
+++ radix-sort/maximum-gap.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        if(nums.size()<=2)return 0;
+        if(nums.size()<2)return 0;
         sort(nums.begin(),nums.end());
 
         int ans=0;

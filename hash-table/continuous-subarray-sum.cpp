74ce7662a1f419247a201cd097471bc082a42723
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-25 17:48:34 +0530

    LC-continuous-subarray-sum [Runtime: N/A; Memory: N/A]

---
+++ hash-table/continuous-subarray-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        int sum=0;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            if(mpp.find(sum%k)!=mpp.end()){
+                if(i-mpp[sum%k]>=2)return true;
+            }
+            else{
+                mpp[sum%k]=i;
+            }
+        }
+        return false;
+    }
+};

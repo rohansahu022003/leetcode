af709aad425963abb284b853f13ae316f49704c7
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-25 13:53:34 +0530

    LC-set-intersection-size-at-least-two [Runtime: N/A; Memory: N/A]

--- array/set-intersection-size-at-least-two.cpp
+++ array/set-intersection-size-at-least-two.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),
-        [](const vector<int>& a, const vector<int>&b){
-            return a[1]<b[1];
-        });
-        unordered_set<int>choosen;
-        for(int i=0; i<intervals.size();i++){
-            int k=0;
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(choosen.empty()){
-                choosen.insert(end);
-                choosen.insert(end-1);
+       sort(intervals.begin(), intervals.end(),[](vector<int>const& a, vector<int>const& b){
+        return a[1]<b[1];
+       });
+
+       vector<int>st;
+
+       for(auto i : intervals){
+        if(st.empty()){
+            st.push_back(i[1]-1);
+            st.push_back(i[1]);
+          
+        }
+        else if(i[0]<st.back())continue;
+        else if(i[0]==st.back())st.push_back(i[1]);
+        else if(i[0]>st.back()){
+            st.push_back(i[1]-1);
+            st.push_back(i[1]);
+           
             }
-           else{
-            for(auto c:choosen){
-                if(c>=start && c<=end)k++;
-            }
-        if(k==1){
-            if(choosen.find(end)!=choosen.end())choosen.insert(end-1);
-            else choosen.insert(end);
-        }
-        else if(k==0){
-        choosen.insert(end);
-        choosen.insert(end-1);
-        }
-           }  
-        }
-        int ans=choosen.size();
-        return ans;
+       }
+       return st.size();
     }
 };

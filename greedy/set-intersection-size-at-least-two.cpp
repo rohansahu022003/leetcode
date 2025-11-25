b7dd5e373cb67cc89b1769dc29b0ec923eea0a8a
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-25 14:03:18 +0530

    LC-set-intersection-size-at-least-two [Runtime: 19 ms; Memory: 23.3 MB]

--- greedy/set-intersection-size-at-least-two.cpp
+++ greedy/set-intersection-size-at-least-two.cpp
@@ -4,23 +4,32 @@ public:
        sort(intervals.begin(), intervals.end(),[](vector<int>const& a, vector<int>const& b){
         return a[1]<b[1];
        });
-
        vector<int>st;
 
        for(auto i : intervals){
+        int k=0;
         if(st.empty()){
             st.push_back(i[1]-1);
             st.push_back(i[1]);
           
         }
-        else if(i[0]<st.back())continue;
-        else if(i[0]==st.back())st.push_back(i[1]);
-        else if(i[0]>st.back()){
+       else {
+        for(auto s: st){
+            if(i[0]<=s && i[1]>=s)k++;
+        }
+            if(k==1){
+                if(find(st.begin(),st.end(),i[1])!=st.end())st.push_back(i[1]-1);
+                else st.push_back(i[1]);
+            }
+            else if(k==0){
             st.push_back(i[1]-1);
             st.push_back(i[1]);
-           
             }
+            
+        
+        }
        }
+       
        return st.size();
     }
 };

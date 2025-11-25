2c1ede51693e76284d641499e95abc91d1bc28f2
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-25 18:35:40 +0530

    LC-subarray-sums-divisible-by-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int ans=0,sum=0;
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        for(auto num:nums){
+            sum+=num;
+            int mod=sum%k;
+            ans+=mpp[mod];
+            mpp[mod]++;
+        }
+        return ans;
+    }
+};

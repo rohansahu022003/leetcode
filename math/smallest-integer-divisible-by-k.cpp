8904d3cc0b83e53a7ea139478cfafe722f390723
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-26 00:50:33 +0530

    LC-smallest-integer-divisible-by-k [Runtime: 0 ms; Memory: 7.8 MB]

--- math/smallest-integer-divisible-by-k.cpp
+++ math/smallest-integer-divisible-by-k.cpp
@@ -4,10 +4,10 @@ public:
         if(k==1)return 1;
 
         if(k%2==0 || k%5==0)return -1;
-    long long rem=0;
+    int rem=0;
         for(int i=1;i<=k; i++){
-            rem=((rem*10)+1);
-            if(rem%k==0)return i;
+            rem=((rem*10)+1)%k;
+            if(rem==0)return i;
         }
         return -1;
     }

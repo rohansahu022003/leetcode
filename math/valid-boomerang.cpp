d4c9c3c221d0c58ec70a7c38cd6561ea0d0effc8
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-27 01:43:22 +0530

    LC-valid-boomerang [Runtime: N/A; Memory: N/A]

--- math/valid-boomerang.cpp
+++ math/valid-boomerang.cpp
@@ -3,7 +3,9 @@ public:
     bool isBoomerang(vector<vector<int>>& points) {
         double m=(double(points[1][1])-points[0][1])/(double(points[1][0])-points[0][0]);
         double n=(double(points[2][1])-points[1][1])/(double(points[2][0])-points[1][0]);
-
+        set<vector<int>>st;
+        for(auto p:points)st.insert(p);
+        if(st.size()<=2)return false;
         return m!=n;
     }
 };

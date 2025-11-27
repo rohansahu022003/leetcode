5e7d05640ed53d35c6ae748edd2f4caeb42d53d2
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-27 16:48:23 +0530

    LC-maximum-subarray-sum-with-length-divisible-by-k [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+       unordered_map<long,long long>maxsum;
+       long long sum=0;
+       maxsum[0]=0;
+       long long ans=LLONG_MIN;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            if(!maxsum.count((i+1)%k)){
+                maxsum[(i+1)%k]=sum;
+            }
+            else{
+                int m=sum-maxsum[(i+1)%k];
+            if(m>ans)ans=m;
+                if(m<maxsum[(i+1)%k]){
+                    maxsum[(i+1)%k]=sum;
+                }
+
+            }
+        }
+        return ans;
+
+    }
+};

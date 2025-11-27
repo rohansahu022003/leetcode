065588e71201482f0c10fac7f11215fbbd99c162
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-28 03:32:31 +0530

    LC-coin-change [Runtime: N/A; Memory: N/A]

--- dynamic-programming/coin-change.cpp
+++ dynamic-programming/coin-change.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,INT_MAX);
-        dp[0]=0;
-
-        for(int i=1; i<=amount; i++){
-            for(auto coin:coins){
-                if(i-coin>=0 && dp[i-coin]!=INT_MAX){
-                    dp[i]=min(dp[i],1+dp[i-coin]);
-                }
-            }
+       int ans=0;
+       sort(coins.begin(), coins.end());
+       for(int i=coins.size()-1; i>=0;i--){
+        while(amount-coins[i]>=0){
+            amount-=coins[i];
+            ans++;
         }
-        return (dp[amount]==INT_MAX?-1:dp[amount]);
+       }
+       if(amount)return -1;
+       return ans;
     }
 };

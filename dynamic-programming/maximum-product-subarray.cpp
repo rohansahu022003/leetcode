0e5fb7fbdc2e9723c016a329342318b8e303d7b2
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-28 12:38:43 +0530

    LC-maximum-product-subarray [Runtime: 0 ms; Memory: 17.8 MB]

--- dynamic-programming/maximum-product-subarray.cpp
+++ dynamic-programming/maximum-product-subarray.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
        int ans=INT_MIN;
-       int m=1;
-       int n=nums.size();
 
-       for(int i=0; i<n;i++){
-        m*=nums[i];
-        ans=max(m,ans);
-        if(m==0)m=1;
+       int n=nums.size();
+       int prod=1;
+       for(int i=0; i<n; i++){
+       prod*=nums[i];
+       ans=max(prod,ans);
+       if(prod==0)prod=1;
        }
-       m=1;
+       prod=1;
        for(int i=n-1; i>=0; i--){
-        m*=nums[i];
-        ans=max(ans,m);
-        if(m==0)m=1;
+        prod*=nums[i];
+        ans=max(ans,prod);
+        if(prod==0)prod=1;
        }
-       
-       
        return ans;
     }
 };

c9ff7438b56acd33e0647b25bb6846e5a82bc56a
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-11-28 12:36:06 +0530

    LC-maximum-subarray-sum-with-length-divisible-by-k [Runtime: 84 ms; Memory: 188.5 MB]

--- array/maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ array/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
-       unordered_map<long,long long>maxsum;
-       long long sum=0;
-       maxsum[0]=0;
-       long long ans=LLONG_MIN;
-        for(int i=0; i<nums.size(); i++){
-            sum+=nums[i];
-            if(!maxsum.count((i+1)%k)){
-                maxsum[(i+1)%k]=sum;
-            }
-            else{
-                long long m=sum-maxsum[(i+1)%k];
-            if(m>ans)ans=m;
-                if(sum<maxsum[(i+1)%k]){
-                    maxsum[(i+1)%k]=sum;
-                }
+      unordered_map<long long,long long>freq;
+      long long ans=LLONG_MIN;
+      long long sum=0;
 
-            }
+      freq[0]=0;
+
+      for(int i=0; i<nums.size(); i++){
+        sum+=nums[i];
+
+        if(!freq.count((i+1)%k))freq[(i+1)%k]=sum;
+        else{
+        long long m=sum-freq[(1+i)%k];
+        ans=max(ans,m);
+        if(sum<freq[(1+i)%k]){
+            freq[(1+i)%k]=sum;
         }
-        return ans;
+        }
+       
+
+      }
+      return ans;
 
     }
 };

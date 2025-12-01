3bbc7fc9d65a5ce4e9a62c9bc10921e97071ed2e
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-01 23:04:51 +0530

    LC-shuffle-the-array [Runtime: 4 ms; Memory: 13.3 MB]

---
+++ array/shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int i=0;
+        int j=n;
+        vector<int>ans;
+        while(j<nums.size()){
+         ans.push_back(nums[i++]);
+         ans.push_back(nums[j++]);
+        }
+
+        return ans;
+    }
+};

6b5968d299962b1e3811ae52fb20ace7270d3f46
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-02 13:20:13 +0530

    LC-next-greater-element-i [Runtime: N/A; Memory: N/A]

---
+++ hash-table/next-greater-element-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        vector<int>ans(n,-1);
+        stack<int>st;
+        unordered_map<int,int>next;
+
+        for(auto num: nums2){
+            if(!st.empty() && st.top()<num){
+                next[st.top()]=num;
+                st.pop();
+            }
+            st.push(num);
+        }
+
+        for(int i=0; i<nums1.size(); i++){
+            if(next.count(nums1[i]))ans[i]=next[nums1[i]];
+        }
+        return ans;
+    }
+};

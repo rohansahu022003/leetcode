6fd04fc8b21910399d3321b2b14b1cb3d3fc068f
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-02 05:56:06 +0530

    LC-plus-one [Runtime: 0 ms; Memory: 11.2 MB]

--- math/plus-one.cpp
+++ math/plus-one.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
         vector<int>ans;
-        int j=1;
-        for(int i=digits.size()-1; i>=0; i--){
-            int d=digits[i]+j;
-            if(d<10){
-                ans.push_back(d);
-                j=0;
-            }
-            else{
-                ans.push_back(d%10);
-            }
+        int a =digits[n-1]+1;
+        ans.push_back(a%10);
+        int carry=a/10;
+        for(int i=n-2; i>=0; i--){
+            a=digits[i]+carry;
+            carry=0;
+            ans.push_back(a%10);
+            carry=a/10;
         }
-        if(j==1)ans.push_back(1);
-        reverse(ans.begin(),ans.end());
+        if(carry)ans.push_back(carry);
+
+        reverse(ans.begin(), ans.end());
         return ans;
+        
     }
 };

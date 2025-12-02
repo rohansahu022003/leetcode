efdf969802b6a63dadbdd788e7863a8660312644
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-02 17:38:12 +0530

    LC-next-greater-element-iii [Runtime: 0 ms; Memory: 7.9 MB]

--- math/next-greater-element-iii.cpp
+++ math/next-greater-element-iii.cpp
@@ -3,24 +3,30 @@ public:
     int nextGreaterElement(int n) {
         string s=to_string(n);
         int l=s.size();
-        int j;
-int ans=-1;
+        int pivot=-1;
+        int pivotvalue=0;
+long long ans=-1;
         for(int i=l-1; i>0 ; i--){
-            j=i;
          int a=s[i]-'0';
          int b=s[i-1]-'0';
          if(a>b){
-          swap(s[i],s[i-1]);
-         sort(s.begin()+i, s.end());
-         long long num=stoll(s);
-         if(num<INT_MAX){
-           
-            ans=num;
+         pivot=i-1;
+         break;
+         }
+        }
+        if(pivot==-1)return -1;
+        pivotvalue=s[pivot]-'0';
+        for(int i=l-1; i>pivot; i-- ){
+         int a=s[i]-'0';
+         if(a>pivotvalue){
+            swap(s[pivot], s[i]);
             break;
          }
-         }
-         
         }
-    return ans;
+        reverse(s.begin()+pivot+1, s.end());
+        ans=stoll(s);
+        cout<<ans;
+        if(ans>INT_MAX)return -1;
+    return (int)ans;
     }
 };

7cfb2d96f0f27dfd960781ac88b76f81502d0e8b
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-02 12:59:30 +0530

    LC-count-number-of-trapezoids-i [Runtime: N/A; Memory: N/A]

--- array/count-number-of-trapezoids-i.cpp
+++ array/count-number-of-trapezoids-i.cpp
@@ -1,12 +1,4 @@
 class Solution {
-    public:
-    int factorial(int i){
-        int res=1;
-        for(int j=2; j<=i; j++){
-            res*=j;
-        }
-        return res;
-    }
 public:
     int countTrapezoids(vector<vector<int>>& points) {
         unordered_map<int,int>level;
@@ -16,17 +8,12 @@ int const mod=10e9+7;
             level[i]++;
         }
         int ans=0;
-        vector<int>pairs;
-        for(auto l:level){
-            if(l.second<2)continue;
-            int i=factorial(l.second)/2*factorial(l.second-2));
-            pairs.push_back(i);
-        }
-
-        for(int i=0; i<pairs.size(); i++){
-            for(int j=i+1; j<pairs.size(); j++){
-                ans=ans+(pairs[i]*pairs[j])%mod;
-            }
+        int sum=0;
+        for(auto [p,t]:level){
+            if(t<2)continue;
+            int c=((t*(t-1))/2);
+            ans=(ans+(sum*c))%mod;
+            sum+=c;
         }
         return ans;
     }

5ee3f9febdff9565d5597ed7dabe9b651a699ce0
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-02 13:47:24 +0530

    LC-next-greater-element-iii [Runtime: N/A; Memory: N/A]

--- two-pointers/next-greater-element-iii.cpp
+++ two-pointers/next-greater-element-iii.cpp
@@ -3,14 +3,18 @@ public:
     int nextGreaterElement(int n) {
         string s=to_string(n);
         int l=s.size();
+        int j;
 int ans=-1;
         for(int i=l-1; i>0 ; i--){
+            j=i;
          int a=s[i]-'0';
          int b=s[i-1]-'0';
          if(a>b){
           swap(s[i],s[i-1]);
+         sort(s.begin()+i, s.end());
          long long num=stoll(s);
          if(num<INT_MAX){
+           
             ans=num;
             break;
          }

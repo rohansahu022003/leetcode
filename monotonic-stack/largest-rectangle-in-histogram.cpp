68b9526af8e66f579317ddf88efb873cd6d1a11e
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-02 05:42:23 +0530

    LC-largest-rectangle-in-histogram [Runtime: 16 ms; Memory: 81.3 MB]

--- monotonic-stack/largest-rectangle-in-histogram.cpp
+++ monotonic-stack/largest-rectangle-in-histogram.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& h) {
-        int n=h.size();
-        deque<int>st;
-        int ans=0;
-        int low=INT_MAX;
-        int j=1;
-        for(int i=0; i<h.size(); i++){
-            if(st.empty() || st.back()<=h[i])st.push_back(h[i]);
-            else{
-                int a;
-                a=h[i]*(st.size()+1);
-                ans=max(ans,a);
-            while(st.size() && st.back()>h[i]){
-                a=st.front()*st.size();
-             ans=max(ans, a);
-             st.pop_front();
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int area = 0, n = heights.size();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[i] < heights[st.top()]) {
+                int bar = st.top(); st.pop();
+                int pse = st.empty() ? -1 : st.top();
+                int nse = i;
+                area = max(area, heights[bar] * (nse - pse - 1));
             }
-        if(h[i]>0)st.push_back(h[i]);
-            }
-           low=min(low,h[i]);
-           ans=max(ans,low*j);
-           j++;
-           if(h[i]==0){
-            low=INT_MAX;
-            j=1;
-           }
-        }
-        while(!st.empty()){
-            int a=st.front()*st.size();
-             ans=max(ans, a);
-             st.pop_front();
+            st.push(i);
         }
 
-        return ans;
+        while (!st.empty()) {
+            int bar = st.top(); st.pop();
+            int pse = st.empty() ? -1 : st.top();
+            int nse = n;
+            area = max(area, heights[bar] * (nse - pse - 1));
+        }
+
+        return area;
     }
 };

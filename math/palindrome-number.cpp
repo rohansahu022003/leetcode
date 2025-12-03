cc5e962cabd27c0f8d43d73ed677ec47dd548d0c
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-03 18:45:05 +0530

    LC-palindrome-number [Runtime: 6 ms; Memory: 10.9 MB]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -7,7 +7,7 @@ public:
         int i=0;
         int j=s.size()-1;
         while(i<s.size()){
-         xo=s[i++]^s[j--];
+         xo+=s[i++]^s[j--];
         }
         return (xo==0?true:false);
     }

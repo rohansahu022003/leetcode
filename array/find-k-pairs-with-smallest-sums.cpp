067ea04dd471acaccf8df596ef3b4c6f530cccbf
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-06 01:19:54 +0530

    LC-find-k-pairs-with-smallest-sums [Runtime: N/A; Memory: N/A]

--- array/find-k-pairs-with-smallest-sums.cpp
+++ array/find-k-pairs-with-smallest-sums.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
      vector<vector<int>>ans;
-  priority_queue<int,vector<int>,greater<int>>pq1;
-  priority_queue<int,vector<int>,greater<int>>pq2;
+  using T=pair<int,pair<int,int>>;
+  priority_queue<T,vector<T>,greater<T>>minimum;
 
-  for(auto num: nums1)pq1.push(num);
-  for(auto num: nums2)pq2.push(num);
-  int x=pq1.top();
-  pq1.pop();
-  int y=pq2.top();
-  pq2.pop();
-  ans.push_back({x,y});
-  while(k-1){
-   if(pq1.top()>=pq2.top()){
-    y=pq2.top();
-    pq2.pop();
-   }
-   else{
-    x=pq1.top();
-    pq1.pop();
-   }
-   ans.push_back({x,y});
-   k--;
+  minimum.push({nums1[0]+nums2[0],{0,0}});
+
+  while(k){
+    int i=minimum.top().second.first;
+    int j=minimum.top().second.second;
+    minimum.pop();
+    minimum.push({nums1[i]+nums2[j+1],{i,j+1}});
+    if(j==0){
+        minimum.push({nums1[i+1]+nums2[j],{i+1,j}});
+    }
+    ans.push_back({nums1[i],nums2[j]});
+    k--;
   }
+  
   return ans;
     }
 };

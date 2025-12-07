b6dcb155c69039febc7ae74ac94127e64b72f095
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-07 19:43:05 +0530

    LC-count-odd-numbers-in-an-interval-range [Runtime: 3 ms; Memory: 7.9 MB]

--- math/count-odd-numbers-in-an-interval-range.cpp
+++ math/count-odd-numbers-in-an-interval-range.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int ans=0;
-        for(int i=low; i<=high; i++){
-            if(i%2!=0)ans++;
-        }
-        return ans;
+       return (high+1)/2 -(low/2);
     }
 };

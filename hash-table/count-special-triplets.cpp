654b3324749837ad4a5af490d198f6b8c25f9d42
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-09 11:58:31 +0530

    LC-count-special-triplets [Runtime: N/A; Memory: N/A]

--- hash-table/count-special-triplets.cpp
+++ hash-table/count-special-triplets.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int,int>freq;
+        unordered_map<int,vector<int>>freq;
         int ans=0;
-        for(auto num: nums){
-           
-           if(num==0 ){
-
-           if(freq[0]>1) ans+=freq[0]-1;
-           }
-           else if(freq.count(num)){
-               if(freq.count(num/2))ans+=freq[num/2];
+        for(int i=0; i<nums.size(); i++){
+             freq[nums[i]].push_back(i);
+        }
+        for(auto f: freq){
+            if(f.first==0){
+                 if(freq.count(f.first*2)){
+             for(auto i:f.second){
+                int right=0;
+                for(auto j: freq[f.first*2]){
+                    if(j>=i)right++;
+                }
+                int left=freq[f.first*2].size()-right;
+                right--;
+             ans+=right*left;
             }
-             freq[num]++;
+                 }
+            }
+            else{
+            if(freq.count(f.first*2)){
+             for(auto i:f.second){
+                int right=0;
+                for(auto j: freq[f.first*2]){
+                    if(j>i)right++;
+                }
+                int left=freq[f.first*2].size()-right;
+             ans+=right*left;
+             }
+            
+            }
+            }
+            
+        }
         }
         return ans;
     }

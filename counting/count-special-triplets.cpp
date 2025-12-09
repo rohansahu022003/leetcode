f3cf63bb11ffde93fbe927c9035fe8f3b2bd4210
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-09 12:18:22 +0530

    LC-count-special-triplets [Runtime: N/A; Memory: N/A]

--- counting/count-special-triplets.cpp
+++ counting/count-special-triplets.cpp
@@ -6,36 +6,17 @@ public:
         for(int i=0; i<nums.size(); i++){
              freq[nums[i]].push_back(i);
         }
-        for(auto f: freq){
-            if(f.first==0){
-                 if(freq.count(f.first*2)){
-             for(auto i:f.second){
-                int right=0;
-                for(auto j: freq[f.first*2]){
-                    if(j>=i)right++;
-                }
-                int left=freq[f.first*2].size()-right;
-                right--;
-             ans+=right*left;
-            }
-                 }
-            }
-            else{
-            if(freq.count(f.first*2)){
-             for(auto i:f.second){
-                int right=0;
-                for(auto j: freq[f.first*2]){
-                    if(j>i)right++;
-                }
-                int left=freq[f.first*2].size()-right;
-             ans+=right*left;
-             }
-            
-            }
-            }
-            
-        }
-        }
+       for(int j=0; j<nums.size(); j++){
+        int m=nums[j];
+        int target=m*2;
+
+        if(!freq.count(target))continue;
+
+        auto &vb=freq[target];
+        int left=lower_bound(vb.begin(),vb.end(),j)-vb.begin();
+        int right=vb.end()-upper_bound(vb.begin(),vb.end(),j);
+        ans+=left*right;
+       }
         return ans;
     }
 };

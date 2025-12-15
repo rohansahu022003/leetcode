c268eba4d7afbbc42b5b0742c275c9eeb89d7d23
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-16 05:01:25 +0530

    LC-number-of-smooth-descent-periods-of-a-stock [Runtime: 7 ms; Memory: 107 MB]

--- array/number-of-smooth-descent-periods-of-a-stock.cpp
+++ array/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
-        int ans=prices.size();
+        long long ans=prices.size();
 
-        int k=1;
+        long long k=1;
 
         for(int i=0; i<prices.size()-1; i++){
             if(prices[i]-prices[i+1]==1){

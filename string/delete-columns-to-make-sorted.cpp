fab877ee65047b3a49dd1dd7c2aba24dcde45031
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-20 22:24:35 +0530

    LC-delete-columns-to-make-sorted [Runtime: 0 ms; Memory: 15.6 MB]

--- string/delete-columns-to-make-sorted.cpp
+++ string/delete-columns-to-make-sorted.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
         int n=strs[0].size();
+        int m=strs.size();
         int ans=0;
         for(int i=0; i<n; i++){
-            for(int j=1; j<strs.size(); j++){
+            for(int j=1; j<m; j++){
              if(strs[j-1][i]>strs[j][i]){
                 ans++;
                 break;

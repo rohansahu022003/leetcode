9c9270b277898efc51533d75dd242648066ff7b9
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-21 23:34:36 +0530

    LC-delete-columns-to-make-sorted-ii [Runtime: 0 ms; Memory: 12.9 MB]

--- array/delete-columns-to-make-sorted-ii.cpp
+++ array/delete-columns-to-make-sorted-ii.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int k=strs[0].size();
-        int n=strs.size();
-        int ans=0;
-        for(int i=0; i<k;i++){
-            if(ans!=i)break;
-            for(int j=1; j<n; j++){
-             
-             if(strs[j-1][i]>strs[j][i]){
-               ans++;
-               break;
-             }
+        int n = strs.size();
+        int m = strs[0].size();
+
+        // resolved[i] indicates strs[i] < strs[i+1] is finalized
+        vector<bool> resolved(n - 1, false);
+        int unresolved = n - 1;
+        int deletions = 0;
+
+        for (int col = 0; col < m && unresolved > 0; col++) {
+            bool bad = false;
+
+            // Check for lexicographical violation
+            for (int i = 0; i < n - 1; i++) {
+                if (!resolved[i] && strs[i][col] > strs[i + 1][col]) {
+                    bad = true;
+                    break;
+                }
+            }
+
+            if (bad) {
+                deletions++;
+                continue;
+            }
+
+            // Lock in strictly ordered row pairs
+            for (int i = 0; i < n - 1; i++) {
+                if (!resolved[i] && strs[i][col] < strs[i + 1][col]) {
+                    resolved[i] = true;
+                    unresolved--;
+                }
             }
         }
-        return ans;
+
+        return deletions;
     }
 };

c6eeba8f56aa6aa6d4d7033404b81a27895ce234
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-25 16:45:51 +0530

    LC-maximize-happiness-of-selected-children [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end());
+
+        int ans=0;
+        int t=0;
+        int n=happiness.size()-1;
+        for(int i=0; i<k;i++){
+            ans+=happiness[n--]-t;
+            t++;
+        }
+
+        return ans;
+    }
+};

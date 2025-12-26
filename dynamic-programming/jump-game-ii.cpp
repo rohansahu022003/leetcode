64a9f0f55b208daf7b85d1327627992738ba7a85
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-27 00:09:09 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game-ii.cpp
+++ dynamic-programming/jump-game-ii.cpp
@@ -1,23 +1,19 @@
-
 class Solution {
 public:
-
     int jump(vector<int>& nums) {
+   
+   for(int i=1; i<nums.size(); i++){
+    nums[i]=max(nums[i]+1, nums[i-1]);
+   }
+   int ind=0;
+   int ans=0;
 
-      for(int i = 1; i < nums.size(); i++)
-      {
-        nums[i] = max(nums[i] + i, nums[i-1]);
-      }
+   while(ind<nums.size()-1){
+    ans++;
+    ind=nums[ind];
+   }
 
-      int ind = 0;
-      int ans = 0;
-
-      while(ind < nums.size() - 1)
-      {
-        ans++;
-        ind = nums[ind];
-      }
-
-      return ans;
+   return ans;
+       
     }
 };

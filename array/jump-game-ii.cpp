328955b52252bda16cb38b1a12024df54887687c
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-27 00:10:07 +0530

    LC-jump-game-ii [Runtime: 0 ms; Memory: 20.4 MB]

--- array/jump-game-ii.cpp
+++ array/jump-game-ii.cpp
@@ -3,7 +3,7 @@ public:
     int jump(vector<int>& nums) {
    
    for(int i=1; i<nums.size(); i++){
-    nums[i]=max(nums[i]+1, nums[i-1]);
+    nums[i]=max(nums[i]+i, nums[i-1]);
    }
    int ind=0;
    int ans=0;

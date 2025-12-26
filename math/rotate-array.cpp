9dade9c8e0081ab9e2c1b0fce302f4edb2d3abc6
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-26 23:20:59 +0530

    LC-rotate-array [Runtime: 4 ms; Memory: 30.6 MB]

--- math/rotate-array.cpp
+++ math/rotate-array.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k=k%nums.size();
-      reverse(nums.begin(), nums.begin()+nums.size()-k);
-      reverse(nums.begin()+nums.size()-k, nums.end());
-      reverse(nums.begin(),nums.end());
+     int n=nums.size();
+       vector<int>ans(n);
+       for(int i=0; i<n;i++){
+        ans[(i+k)%n]=nums[i];
+       }
+       nums=ans;
     }
 };

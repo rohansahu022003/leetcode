6f2f925bc895fb498d35c7592bd7a4ff27e977d5
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-27 23:05:33 +0530

    LC-h-index [Runtime: N/A; Memory: N/A]

--- array/h-index.cpp
+++ array/h-index.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+        if(citations.size()==1)return 1;
     sort(citations.begin(), citations.end());
     int n=citations.size();
     int ans=0;

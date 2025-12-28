192de7a54045d03a8c3ecc8ce557fc232550d4be
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-28 22:12:14 +0530

    LC-isomorphic-strings [Runtime: 4 ms; Memory: 9.3 MB]

--- hash-table/isomorphic-strings.cpp
+++ hash-table/isomorphic-strings.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>mapping;
-
+        unordered_map<char,char>mapping,map;
+    
         for(int i=0; i<s.size(); i++){
             if(mapping.find(s[i])!=mapping.end()){
                 if(mapping[s[i]]!=t[i])return false;
             }
-            else mapping[s[i]]=t[i];
+            if(map.find(t[i])!=map.end()){
+                if(map[t[i]]!=s[i])return false;
+            }
+            else {
+                mapping[s[i]]=t[i];
+                map[t[i]]=s[i];
+            }
         }
 
         return true;

2284e3a4b13333b84b063b0d7b9519f4dbdaffb9
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2025-12-28 22:48:56 +0530

    LC-valid-anagram [Runtime: 7 ms; Memory: 9.8 MB]

--- sorting/valid-anagram.cpp
+++ sorting/valid-anagram.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())return false;
-
-        unordered_map<char,int>freq;
-        for(int i=0; i<s.size(); i++){
-            freq[s[i]]++;
-        }
-        for(int i=0; i<t.size(); i++){
-            freq[t[i]]--;
-            if(freq[t[i]]==0)freq.erase(t[i]);
-        }
-        return (freq.empty());
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return (s==t);
     }
 };

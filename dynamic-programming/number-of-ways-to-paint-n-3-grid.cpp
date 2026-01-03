f0e7279e3a2a1f1744e0022d1420bf9151c9eb66
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-01-03 22:39:56 +0530

    LC-number-of-ways-to-paint-n-3-grid [Runtime: 0 ms; Memory: 7.8 MB]

---
+++ dynamic-programming/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int MOD=1e9+7;
+        long long x=6;
+        long long y=6;
+
+        for(int i=2; i<=n; i++){
+            long long new_x=(3*x + 2*y)%MOD;
+            long long new_y=(2*x+2*y)%MOD;
+
+            x=new_x;
+            y=new_y;
+        }
+
+        return (x+y)%MOD;
+    }
+};

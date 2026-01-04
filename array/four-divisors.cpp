da87e402093fdbc1b8f6f71a9a1421a5b9a32ad6
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-01-04 07:07:05 +0530

    LC-four-divisors [Runtime: N/A; Memory: N/A]

---
+++ array/four-divisors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans=0;
+        for(auto n: nums){
+            unordered_set<int>st;
+            for(int i=1; i<n/2; i++){
+                if(n%i==0){
+                    st.insert(i);
+                    st.insert(n/i);
+                }
+            }
+            if(st.size()==4){
+             for(auto s: st){
+                ans+=s;
+             }
+            }
+        }
+
+        return ans;
+    }
+};

841f8231f57f7fbde28ef382d224ea8ff696a854
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-01-04 07:08:01 +0530

    LC-four-divisors [Runtime: N/A; Memory: N/A]

--- math/four-divisors.cpp
+++ math/four-divisors.cpp
@@ -4,7 +4,7 @@ public:
         int ans=0;
         for(auto n: nums){
             unordered_set<int>st;
-            for(int i=1; i<n/2; i++){
+            for(int i=1; i<sqrt(n); i++){
                 if(n%i==0){
                     st.insert(i);
                     st.insert(n/i);

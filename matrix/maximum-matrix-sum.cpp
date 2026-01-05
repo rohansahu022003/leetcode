426fb7e8acf5b4ba528d7a0489f40d0bdf37622a
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-01-05 15:55:40 +0530

    LC-maximum-matrix-sum [Runtime: N/A; Memory: N/A]

--- matrix/maximum-matrix-sum.cpp
+++ matrix/maximum-matrix-sum.cpp
@@ -3,22 +3,31 @@ public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         int ans=0;
         vector<int>negative;
-
+int lowest=INT_MAX;
         for(int i=0; i<matrix.size(); i++){
             for(int j=0; j<matrix[i].size(); j++){
+                
                 if(matrix[i][j]<=0)negative.push_back(matrix[i][j]);
-                else ans+=matrix[i][j];
+                else{
+                    lowest=min(lowest, matrix[i][j]);
+                    ans+=matrix[i][j];
+                } 
             }
         }
+        int n=negative.size();
 sort(negative.begin(), negative.end());
-        if(negative.size()%2==0){
-            for(int i=0; i<negative.size(); i++){
+        if(n%2==0){
+            for(int i=0; i<n; i++){
                 ans+=abs(negative[i]);
             }
         }
     else{
-        for(int i=0; i<negative.size()-1; i++)ans+=abs(negative[i]);
-        ans+=negative[negative.size()-1];
+        for(int i=0; i<n-1; i++)ans+=abs(negative[i]);
+        if(abs(negative[n-1])<=lowest)ans+=negative[n-1];
+        else{
+            ans+=abs(negative[n-1]);
+            ans-=2*lowest;
+        }
     }
   
     return ans;

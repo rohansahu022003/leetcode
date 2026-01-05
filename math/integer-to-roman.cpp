9ee6f2d6a5cfc6e178ca609f368957b5dd7b49cf
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-01-05 18:43:50 +0530

    LC-integer-to-roman [Runtime: 7 ms; Memory: 12.8 MB]

--- math/integer-to-roman.cpp
+++ math/integer-to-roman.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string intToRoman(int num) {
         vector<pair<string,int>>symbols{
-            {"M", 1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"x",10},{"IX",9},{"V",5},{"IV",4},{"I",1}
+            {"M", 1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}
         };
 
 string ans;

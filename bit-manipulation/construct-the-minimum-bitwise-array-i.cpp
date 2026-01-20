d20f363947f71ea29dce3026054c0009898ae865
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-01-21 02:45:34 +0530

    LC-construct-the-minimum-bitwise-array-i [Runtime: 3 ms; Memory: 25.1 MB]

---
+++ bit-manipulation/construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int>ans(nums.size(),-1);
+        for(int i=0; i<nums.size(); i++){
+          
+           for(int j=0; j<=1000; j++){
+            if((j | j+1) ==nums[i]){
+                ans[i]=j;
+                break;
+            }
+           }
+        }
+        return ans;
+    }
+};

a0ab29e7fe952a4c28408547628f5f53ebe12a4f
rohan sahu <161516796+rohansahu022003@users.noreply.github.com>
2026-03-06 06:30:36 +0530

    LC-check-if-binary-string-has-at-most-one-segment-of-ones [Runtime: N/A; Memory: N/A]

--- string/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ string/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -4,13 +4,16 @@ public:
         if(s.length()==1 || s.length()==2)return true;
         int x=stoi(s);
         int y=0;
+        int count=0;
         while(x){
            int digit=x%10;
+           if(digit==1)count++;
            if(y==1 && digit==1)return true;
            if(digit==1)y=1;
            else y=0;
            x/=10;
         }
+        if(count==1)return true;
         return false;
     }
 };
